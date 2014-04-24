3e61483df6654f16ec6d52529c2de9f0b4598449
Marko Lyly <Marko.Lyly@proactum.fi>
2014-04-24 10:47:50 +0300

    OVT-7591 Jos käyttäjällä ei ole attribuutteja tullee 500 - NPE "/cas/me" urista-> tarjonta ei toimi. Lisätty defensiivistä koodausta, ravistettu - ei sekoitettu.

--- cas-virkailija/src/main/java/fi/vm/sade/auth/ldap/LdapUserAttributeMapper.java
+++ cas-virkailija/src/main/java/fi/vm/sade/auth/ldap/LdapUserAttributeMapper.java
@@ -1,5 +1,8 @@
 package fi.vm.sade.auth.ldap;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.naming.NamingException;
 import javax.naming.directory.Attributes;
 
@@ -7,14 +10,34 @@ import org.springframework.ldap.core.AttributesMapper;
 
 public class LdapUserAttributeMapper implements AttributesMapper {
 
+    private static final Logger LOG = LoggerFactory.getLogger(LdapUserImporter.class);
+
     @Override
     public Object mapFromAttributes(Attributes attrs) throws NamingException {
         LdapUser user = new LdapUser();
-        user.setFirstName(attrs.get("cn").get().toString());
-        user.setLastName(attrs.get("sn").get().toString());
-        user.setLang(attrs.get("preferredLanguage").get().toString());
-        user.setOid(attrs.get("employeeNumber").get().toString());
+        user.setFirstName(getAttributeValueAsString(attrs, "cn", null));
+        user.setLastName(getAttributeValueAsString(attrs, "sn", null));
+        user.setLang(getAttributeValueAsString(attrs, "preferredLanguage", "fi"));
+        user.setOid(getAttributeValueAsString(attrs, "employeeNumber", null));
         return user;
     }
 
+    private String getAttributeValueAsString(Attributes attrs, String key, String defaultValueIfNull) throws NamingException {
+        if (attrs == null) {
+            LOG.warn("getAttributeValueAsString() - user attributes == NULL");
+            return defaultValueIfNull;
+        }
+
+        if (attrs.get(key) == null) {
+            LOG.warn("getAttributeValueAsString() - user attribute with key {} == NULL", key);
+            return defaultValueIfNull;
+        }
+
+        if (attrs.get(key).get() == null) {
+            LOG.warn("getAttributeValueAsString() - user attribute value with key {} == NULL", key);
+            return defaultValueIfNull;
+        }
+
+        return attrs.get(key).get().toString();
+    }
 }
