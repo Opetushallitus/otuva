556361069e7ac8e723150e45d522cf8de04c0dc3
hkulmala <henri.kulmala@gofore.com>
2014-08-06 15:12:57 +0300

    KJHH-50: POC-testin typo fixi.

--- cas-virkailija/src/main/java/fi/vm/sade/shibboleth/action/ShibbolethAuthenticationHandler.java
+++ cas-virkailija/src/main/java/fi/vm/sade/shibboleth/action/ShibbolethAuthenticationHandler.java
@@ -27,7 +27,7 @@ public class ShibbolethAuthenticationHandler extends HttpServlet {
             throw new RuntimeException("Invalid message data");
         }
 
-        response.sendRedirect(/*redirectUrl*/"https://testi.virkailija.opintopolku.fi/cas/login?service=https%3A%2F%2Ftesti.virkailija.opintopolku.fi%2Fregistration-ui%2Fhtml%2Findex.html%23%2Fregister" + "&authToken=" + identity);
+        response.sendRedirect(/*redirectUrl*/"https://itest-virkailija.oph.ware.fi/cas/login?service=https%3A%2F%2Fitest-virkailija.oph.ware.fi%2Fregistration-ui%2Fhtml%2Findex.html%23%2Fregister" + "&authToken=" + identity);
     }
 /*
     public String getRedirectUrl() {
