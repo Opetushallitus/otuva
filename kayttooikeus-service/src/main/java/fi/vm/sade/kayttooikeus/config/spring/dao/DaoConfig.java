e84117df2ae22664195c3192371988e5255cde1d
Tommi Ratamaa <tommi.ratamaa@gmail.com>
2016-09-02 18:46:36 +0300

    - Turhat kommentit pois

--- kayttooikeus-service/src/main/java/fi/vm/sade/kayttooikeus/config/spring/dao/DaoConfig.java
+++ kayttooikeus-service/src/main/java/fi/vm/sade/kayttooikeus/config/spring/dao/DaoConfig.java
@@ -1,23 +1,5 @@
 package fi.vm.sade.kayttooikeus.config.spring.dao;
 
-//import com.googlecode.flyway.core.Flyway;
-//import com.zaxxer.hikari.HikariConfig;
-//import com.zaxxer.hikari.HikariDataSource;
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.context.ApplicationListener;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.DependsOn;
-//import org.springframework.context.event.ContextClosedEvent;
-//import org.springframework.instrument.classloading.InstrumentationLoadTimeWeaver;
-//import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy;
-//import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-//
-//import javax.sql.DataSource;
-//import java.lang.reflect.InvocationTargetException;
-//import java.util.Properties;
-
 import com.googlecode.flyway.core.Flyway;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
