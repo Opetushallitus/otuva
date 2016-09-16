baa285689a48885a2c1c2209f1d6fcbe4a46107e
Risto Autio <risto.autio@dicode.fi>
2016-09-16 13:18:59 +0300

    added logging

--- kayttooikeus-service2/src/main/java/fi.vm.sade.kayttooikeus/Application.java
+++ kayttooikeus-service2/src/main/java/fi.vm.sade.kayttooikeus/Application.java
@@ -1,13 +1,24 @@
 package fi.vm.sade.kayttooikeus;
 
+import com.mangofactory.swagger.plugin.EnableSwagger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
+import javax.annotation.PostConstruct;
 import java.util.Arrays;
 
 @SpringBootApplication
 public class Application{
+
+    private static final Logger logger = LoggerFactory.getLogger(Application.class);
+
+    @PostConstruct
+    public void logSomething() {
+        logger.info("Sample Info Message");
+    }
 
     public static void main(String[] args) throws Exception {
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
--- kayttooikeus-service2/src/main/java/fi.vm.sade.kayttooikeus/controller/TestController.java
+++ kayttooikeus-service2/src/main/java/fi.vm.sade.kayttooikeus/controller/TestController.java
@@ -1,6 +1,9 @@
 package fi.vm.sade.kayttooikeus.controller;
 
+import fi.vm.sade.kayttooikeus.Application;
 import fi.vm.sade.kayttooikeus.service.HenkiloService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,8 +20,11 @@ public class TestController {
     @Autowired
     private HenkiloService henkiloService;
 
+    private static final Logger logger = LoggerFactory.getLogger(Application.class);
+
     @RequestMapping("/test")
     public Map<String,Object> test() {
+        logger.debug("Sample Debug Message");
         Map<String,Object> jsonStatus = new HashMap<>();
         jsonStatus.put("henkilosCount", henkiloService.countHenkilos());
         return jsonStatus;
