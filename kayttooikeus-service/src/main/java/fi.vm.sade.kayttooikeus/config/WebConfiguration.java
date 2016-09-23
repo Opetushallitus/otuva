e8681c432bb3737fac8c08bc3563051e5ba49fa4
Risto Autio <risto.autio@dicode.fi>
2016-09-23 14:07:50 +0300

    switched to swagger2

---
+++ kayttooikeus-service/src/main/java/fi.vm.sade.kayttooikeus/config/WebConfiguration.java
@@ -0,0 +1,23 @@
+package fi.vm.sade.kayttooikeus.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * Created by autio on 23.9.2016.
+ */
+@Configuration
+@EnableSwagger2
+public class WebConfiguration extends WebMvcConfigurerAdapter{
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+}
