741d9a58df8fa1631346496f22ad15f47c0030ef
Risto Autio <risto.autio@dicode.fi>
2016-11-22 11:16:28 +0200

    removed old api

--- kayttooikeus-service/src/main/java/fi/vm/sade/kayttooikeus/controller/HenkiloController.java
+++ kayttooikeus-service/src/main/java/fi/vm/sade/kayttooikeus/controller/HenkiloController.java
@@ -1,6 +1,5 @@
 package fi.vm.sade.kayttooikeus.controller;
 
-import fi.vm.sade.kayttooikeus.dto.HenkiloTyyppi;
 import fi.vm.sade.kayttooikeus.dto.OrganisaatioHenkiloDto;
 import fi.vm.sade.kayttooikeus.dto.OrganisaatioOidsSearchDto;
 import fi.vm.sade.kayttooikeus.dto.permissioncheck.ExternalPermissionService;
@@ -8,7 +7,6 @@ import fi.vm.sade.kayttooikeus.service.HenkiloService;
 import fi.vm.sade.kayttooikeus.service.OrganisaatioHenkiloService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -47,23 +45,6 @@ public class HenkiloController {
     public OrganisaatioHenkiloDto findByOrganisaatioOid(@PathVariable("oid") String henkiloOid,
                                                         @PathVariable("organisaatioOid") String organisaatioOid) {
         return organisaatioHenkiloService.findOrganisaatioHenkiloByHenkiloAndOrganisaatio(henkiloOid, organisaatioOid);
-    }
-
-    //TODO remove after proxy removed from henkilo
-    @ApiOperation(value = "Hakee henkilöitä organisaatioiden ja käyttöoikeuksien mukaan.",
-            notes = "Tämä toteutus on tehty Osoitepalvelua varten, jonka pitää pystyä "
-                    + "hakemaan henkilöitä henkilötyyppien, organisaatioiden sekä"
-                    + "käyttöoikeusryhmien mukaan, tämä toteutus ei ole UI:n käytössä.")
-    @PreAuthorize("hasAnyRole('ROLE_APP_HENKILONHALLINTA_READ',"
-            + "'ROLE_APP_HENKILONHALLINTA_READ_UPDATE',"
-            + "'ROLE_APP_HENKILONHALLINTA_CRUD',"
-            + "'ROLE_APP_HENKILONHALLINTA_OPHREKISTERI')")
-    @RequestMapping(value = "/byOoids", method = RequestMethod.GET)
-    public List<String> findHenkilos(@ApiParam("Henkilötyypin määre") @RequestParam(value = "ht", required = false) HenkiloTyyppi henkiloTyyppi,
-                             @ApiParam("Organisaatiorajoitteet") @RequestParam(value = "ooids", required = false) List<String> ooids,
-                             @ApiParam("Käyttöoikeusryhmä") @RequestParam(value = "kor", required = false) String groupName) {
-        henkiloTyyppi = henkiloTyyppi == null ? HenkiloTyyppi.VIRKAILIJA : henkiloTyyppi;
-        return henkiloService.findHenkilos(new OrganisaatioOidsSearchDto(henkiloTyyppi, ooids, groupName));
     }
 
     @ApiOperation(value = "Hakee henkilöitä organisaatioiden ja käyttöoikeuksien mukaan.",
