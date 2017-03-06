b0c024938e03e5826e649327e6a74e1612818366
Petri Ryhänen <petri.ryhanen@csc.fi>
2017-03-06 12:50:08 +0200

    Fix disabled test.

--- kayttooikeus-service/src/test/java/fi/vm/sade/kayttooikeus/service/it/KayttajatiedotServiceTest.java
+++ kayttooikeus-service/src/test/java/fi/vm/sade/kayttooikeus/service/it/KayttajatiedotServiceTest.java
@@ -6,21 +6,26 @@ import static fi.vm.sade.kayttooikeus.repositories.populate.HenkiloPopulator.hen
 import static fi.vm.sade.kayttooikeus.repositories.populate.KayttajatiedotPopulator.kayttajatiedot;
 
 import fi.vm.sade.kayttooikeus.model.Identification;
+import fi.vm.sade.kayttooikeus.model.Kayttajatiedot;
+import fi.vm.sade.kayttooikeus.repositories.KayttajatiedotRepository;
 import fi.vm.sade.kayttooikeus.service.KayttajatiedotService;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.catchThrowable;
-
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
 
 @RunWith(SpringRunner.class)
 public class KayttajatiedotServiceTest extends AbstractServiceIntegrationTest {
 
     @Autowired
     private KayttajatiedotService kayttajatiedotService;
+
+    @Autowired
+    private KayttajatiedotRepository kayttajatiedotRepository;
 
     @Test
     public void createShouldReturn() {
@@ -50,18 +55,16 @@ public class KayttajatiedotServiceTest extends AbstractServiceIntegrationTest {
     }
 
     @Test
-    @Ignore
     public void testValidateUsernamePassword() throws Exception {
         final String henkiloOid = "1.2.246.562.24.27470134096";
         String username = "eetu.esimerkki@geemail.fi";
         String password = "paSsword&23";
+        populate(henkilo(henkiloOid));
+        populate(kayttajatiedot(henkilo(henkiloOid), username));
         kayttajatiedotService.changePasswordAsAdmin(henkiloOid, password);
-//        Identification identification = kayttajatiedotService.validate(username, password);
-//        assertThat(identification).isNotNull();
-//
-//        Identification identification1 = kayttajatiedotService.validate(username, "notthecorrectpassword");
-//        assertThat(identification1).isNull();
+        Optional<Kayttajatiedot> kayttajatiedot = this.kayttajatiedotRepository.findByUsername(username);
+        assertThat(kayttajatiedot)
+                .isNotEmpty()
+                .hasValueSatisfying(kayttajatiedot1 -> assertThat(kayttajatiedot1.getPassword()).isNotEmpty());
     }
-
-
 }
