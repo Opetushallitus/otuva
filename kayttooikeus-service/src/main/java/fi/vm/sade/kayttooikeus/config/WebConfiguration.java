020236d45bd5cdbd1b11d3cf15cbc9a4b6fc38c4
Petri Ryhänen <petri.ryhanen@csc.fi>
2017-03-23 16:51:47 +0200

    Add localhost origins.

--- kayttooikeus-service/src/main/java/fi/vm/sade/kayttooikeus/config/WebConfiguration.java
+++ kayttooikeus-service/src/main/java/fi/vm/sade/kayttooikeus/config/WebConfiguration.java
@@ -32,7 +32,7 @@ public class WebConfiguration extends WebMvcConfigurerAdapter {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**").allowedOrigins("http://localhost:3000");
     }
 
     @Override
