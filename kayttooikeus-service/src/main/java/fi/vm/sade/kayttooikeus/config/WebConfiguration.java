5214a10b7be0041c4a826fc58c17caebe22613fa
Petri Ryhänen <petri.ryhanen@csc.fi>
2017-03-23 17:38:25 +0200

    Add another localhost option.

--- kayttooikeus-service/src/main/java/fi/vm/sade/kayttooikeus/config/WebConfiguration.java
+++ kayttooikeus-service/src/main/java/fi/vm/sade/kayttooikeus/config/WebConfiguration.java
@@ -32,7 +32,8 @@ public class WebConfiguration extends WebMvcConfigurerAdapter {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000", "http://localhost:8280");
     }
 
     @Override
