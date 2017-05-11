07a2966f2e9353cc3cfc92675b33ffeb0db6b574
Jarkko Pätäri <jarkko.patari@gofore.com>
2017-05-11 08:30:51 +0300

    KJHH-936: Add scheduling

--- kayttooikeus-service/src/main/java/fi/vm/sade/kayttooikeus/config/scheduling/ScheduledTasks.java
+++ kayttooikeus-service/src/main/java/fi/vm/sade/kayttooikeus/config/scheduling/ScheduledTasks.java
@@ -3,7 +3,9 @@ package fi.vm.sade.kayttooikeus.config.scheduling;
 import fi.vm.sade.kayttooikeus.config.properties.CommonProperties;
 import fi.vm.sade.kayttooikeus.service.MyonnettyKayttoOikeusService;
 import fi.vm.sade.kayttooikeus.service.OrganisaatioService;
+import fi.vm.sade.kayttooikeus.service.TaskExecutorService;
 import lombok.RequiredArgsConstructor;
+import org.joda.time.Period;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -19,6 +21,7 @@ public class ScheduledTasks {
     private final OrganisaatioService organisaatioService;
     private final MyonnettyKayttoOikeusService myonnettyKayttoOikeusService;
     private final CommonProperties commonProperties;
+    private final TaskExecutorService taskExecutorService;
 
     @Scheduled(cron = "${kayttooikeus.scheduling.configuration.organisaatiocache}")
     public void updateOrganisaatioCache() {
@@ -28,6 +31,11 @@ public class ScheduledTasks {
     @Scheduled(cron = "${kayttooikeus.scheduling.configuration.vanhentuneetkayttooikeudet}")
     public void poistaVanhentuneetKayttoOikeudet() {
         myonnettyKayttoOikeusService.poistaVanhentuneet(commonProperties.getAdminOid());
+    }
+
+    @Scheduled(cron = "${kayttooikeus.scheduling.configuration.kayttooikeusmuistutus}")
+    public void sendExpirationReminders() {
+        taskExecutorService.sendExpirationReminders(Period.weeks(4), Period.weeks(1));
     }
 
 }
