812602d78c23ba4e55e47177b3f1f9dd0d03d373
Jarkko Pätäri <jarkko.patari@gofore.com>
2017-05-11 08:30:51 +0300

    KJHH-936: Add scheduling

--- kayttooikeus-service/src/main/java/fi/vm/sade/kayttooikeus/config/scheduling/ScheduledTasks.java
+++ kayttooikeus-service/src/main/java/fi/vm/sade/kayttooikeus/config/scheduling/ScheduledTasks.java
@@ -1,7 +1,9 @@
 package fi.vm.sade.kayttooikeus.config.scheduling;
 
 import fi.vm.sade.kayttooikeus.service.OrganisaatioService;
+import fi.vm.sade.kayttooikeus.service.TaskExecutorService;
 import lombok.RequiredArgsConstructor;
+import org.joda.time.Period;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -15,10 +17,16 @@ import org.springframework.stereotype.Component;
 public class ScheduledTasks {
 
     private final OrganisaatioService organisaatioService;
+    private final TaskExecutorService taskExecutorService;
 
     @Scheduled(cron = "${kayttooikeus.scheduling.configuration.organisaatiocache}")
     public void updateOrganisaatioCache() {
         organisaatioService.updateOrganisaatioCache();
+    }
+
+    @Scheduled(cron = "${kayttooikeus.scheduling.configuration.kayttooikeusmuistutus}")
+    public void sendExpirationReminders() {
+        taskExecutorService.sendExpirationReminders(Period.weeks(4), Period.weeks(1));
     }
 
 }
