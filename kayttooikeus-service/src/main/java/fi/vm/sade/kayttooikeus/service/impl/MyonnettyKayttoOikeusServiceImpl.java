d7414a8de1cb5f492fa7e49c736bb1fc701d8698
Jarkko Pätäri <jarkko.patari@gofore.com>
2017-05-11 15:20:18 +0300

    KJHH-934: Use permissionchecker

--- kayttooikeus-service/src/main/java/fi/vm/sade/kayttooikeus/service/impl/MyonnettyKayttoOikeusServiceImpl.java
+++ kayttooikeus-service/src/main/java/fi/vm/sade/kayttooikeus/service/impl/MyonnettyKayttoOikeusServiceImpl.java
@@ -8,7 +8,6 @@ import fi.vm.sade.kayttooikeus.repositories.HenkiloRepository;
 import fi.vm.sade.kayttooikeus.repositories.KayttoOikeusRyhmaTapahtumaHistoriaDataRepository;
 import fi.vm.sade.kayttooikeus.repositories.MyonnettyKayttoOikeusRyhmaTapahtumaDataRepository;
 import fi.vm.sade.kayttooikeus.repositories.MyonnettyKayttoOikeusRyhmaTapahtumaRepository;
-import fi.vm.sade.kayttooikeus.service.KayttajaService;
 import fi.vm.sade.kayttooikeus.service.LdapSynchronization;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -16,6 +15,7 @@ import org.joda.time.LocalDate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import fi.vm.sade.kayttooikeus.service.MyonnettyKayttoOikeusService;
+import fi.vm.sade.kayttooikeus.service.PermissionCheckerService;
 import fi.vm.sade.kayttooikeus.service.exception.DataInconsistencyException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -27,7 +27,7 @@ public class MyonnettyKayttoOikeusServiceImpl implements MyonnettyKayttoOikeusSe
 
     private static final Logger LOGGER = LoggerFactory.getLogger(MyonnettyKayttoOikeusServiceImpl.class);
 
-    private final KayttajaService kayttajaService;
+    private final PermissionCheckerService permissionCheckerService;
     private final HenkiloRepository henkiloRepository;
     private final MyonnettyKayttoOikeusRyhmaTapahtumaRepository myonnettyKayttoOikeusRyhmaTapahtumaRepository;
     private final MyonnettyKayttoOikeusRyhmaTapahtumaDataRepository myonnettyKayttoOikeusRyhmaTapahtumaDataRepository;
@@ -36,7 +36,7 @@ public class MyonnettyKayttoOikeusServiceImpl implements MyonnettyKayttoOikeusSe
 
     @Override
     public void poistaVanhentuneet() {
-        poistaVanhentuneet(kayttajaService.getOid());
+        poistaVanhentuneet(permissionCheckerService.getCurrentUserOid());
     }
 
     @Override
