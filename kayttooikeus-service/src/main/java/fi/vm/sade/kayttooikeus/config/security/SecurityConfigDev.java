e7ea04b56569c5addaf078838bb42ad2432c58a2
Pasi Kallioniemi <pasi.kallioniemi@gofore.com>
2017-07-27 10:30:45 +0300

    Skeleton for receiving shibboleth data in headers

--- kayttooikeus-service/src/main/java/fi/vm/sade/kayttooikeus/config/security/SecurityConfigDev.java
+++ kayttooikeus-service/src/main/java/fi/vm/sade/kayttooikeus/config/security/SecurityConfigDev.java
@@ -35,6 +35,7 @@ public class SecurityConfigDev extends WebSecurityConfigurerAdapter {
                 .antMatchers("/swagger-ui.html").permitAll()
                 .antMatchers("/cas/auth/token/*").permitAll()
                 .antMatchers("/cas/henkilo/*").permitAll()
+                .antMatchers("/cas/tunnistus").permitAll()
                 .antMatchers("/swagger-resources/**").permitAll()
                 .antMatchers("/webjars/springfox-swagger-ui/**").permitAll()
                 .antMatchers("/v2/api-docs").permitAll()
