cc57daec0313f8ea3237c4315b3a5ff51b7d43e8
Petri Ryhänen <petri.ryhanen@csc.fi>
2017-08-15 16:58:48 +0300

    Update /cas/tunnistus to handle loginToken.

--- kayttooikeus-service/src/main/java/fi/vm/sade/kayttooikeus/repositories/TunnistusTokenDataRepository.java
+++ kayttooikeus-service/src/main/java/fi/vm/sade/kayttooikeus/repositories/TunnistusTokenDataRepository.java
@@ -6,8 +6,15 @@ import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
+import java.util.Optional;
+
 @Transactional(propagation = Propagation.MANDATORY)
 @Repository
 public interface TunnistusTokenDataRepository extends CrudRepository<TunnistusToken, Long> {
+    Optional<TunnistusToken> findByAikaleimaGreaterThanAndLoginTokenAndKaytettyIsNull(LocalDateTime aikaleima, String loginToken);
 
+    default Optional<TunnistusToken> findByValidLoginToken(String loginToken) {
+        return findByAikaleimaGreaterThanAndLoginTokenAndKaytettyIsNull(LocalDateTime.now().minusMinutes(10), loginToken);
+    }
 }
