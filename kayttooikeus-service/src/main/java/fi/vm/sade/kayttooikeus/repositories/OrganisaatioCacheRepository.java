2c0264ffd8d7517d2c9e035e276d6e26993321b0
Petri Ryhänen <petri.ryhanen@csc.fi>
2017-09-21 14:40:17 +0300

    KJHH-1079 deprecate OrganisaatioCacheRepository.

--- kayttooikeus-service/src/main/java/fi/vm/sade/kayttooikeus/repositories/OrganisaatioCacheRepository.java
+++ kayttooikeus-service/src/main/java/fi/vm/sade/kayttooikeus/repositories/OrganisaatioCacheRepository.java
@@ -3,5 +3,6 @@ package fi.vm.sade.kayttooikeus.repositories;
 import fi.vm.sade.kayttooikeus.model.OrganisaatioCache;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+@Deprecated
 public interface OrganisaatioCacheRepository extends JpaRepository<OrganisaatioCache, Long>, OrganisaatioCacheRepositoryCustom {
 }
