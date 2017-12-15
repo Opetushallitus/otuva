76bc95284de2199be6b7e13ce9eae3292e8cf7b7
Petri Ryhänen <petri.ryhanen@csc.fi>
2017-12-15 14:41:02 +0200

    KJHH-1137: Update postgres drivers and hikaricp to latest version. Use spring-boot autoconfiguration for configuring datasource and hikaricp.

--- kayttooikeus-service/src/main/java/fi/vm/sade/kayttooikeus/config/db/JpaConfiguration.java
+++ kayttooikeus-service/src/main/java/fi/vm/sade/kayttooikeus/config/db/JpaConfiguration.java
@@ -1,31 +1,13 @@
 package fi.vm.sade.kayttooikeus.config.db;
 
-import com.zaxxer.hikari.HikariDataSource;
-import fi.vm.sade.kayttooikeus.config.properties.HikariProperties;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-import javax.sql.DataSource;
-import java.sql.SQLException;
 
 @Configuration
 @EnableJpaRepositories(basePackages = {"fi.vm.sade.kayttooikeus.repositories"})
 @EntityScan({"fi.vm.sade.kayttooikeus.model"})
 @EnableTransactionManagement
 public class JpaConfiguration {
-    private HikariProperties hikariProperties;
-
-    @Autowired
-    public JpaConfiguration(HikariProperties hikariProperties) {
-        this.hikariProperties = hikariProperties;
-    }
-
-    @Bean
-    public DataSource dataSource() throws SQLException {
-        return new HikariDataSource(hikariProperties);
-    }
 }
