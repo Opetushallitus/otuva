3d02467cd9e9e207502b189d3177f4fc6ae984e7
Jarkko Pätäri <jarkko.patari@gofore.com>
2018-01-22 13:55:38 +0200

    KJHH-1231: Refaktoroi ldap:n ryhmäkäsittelyn

--- kayttooikeus-service/src/main/java/fi/vm/sade/kayttooikeus/config/LdapConfiguration.java
+++ kayttooikeus-service/src/main/java/fi/vm/sade/kayttooikeus/config/LdapConfiguration.java
@@ -1,10 +1,17 @@
 package fi.vm.sade.kayttooikeus.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.ldap.repository.config.EnableLdapRepositories;
+import org.springframework.ldap.core.support.BaseLdapPathBeanPostProcessor;
 
 @Configuration
 @EnableLdapRepositories
 public class LdapConfiguration {
+
+    @Bean
+    public BaseLdapPathBeanPostProcessor baseLdapPathBeanPostProcessor() {
+        return new BaseLdapPathBeanPostProcessor();
+    }
 
 }
