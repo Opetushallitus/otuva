0c09d9bfc2fc8f5dd27599d7b196d51364787f3a
Jarkko Pätäri <jarkko.patari@gofore.com>
2018-01-31 16:12:05 +0200

    KJHH-1250: Korjaa vahvan tunnistaumisen virhetilanteissa

--- kayttooikeus-service/src/main/java/fi/vm/sade/kayttooikeus/service/impl/VahvaTunnistusServiceImpl.java
+++ kayttooikeus-service/src/main/java/fi/vm/sade/kayttooikeus/service/impl/VahvaTunnistusServiceImpl.java
@@ -3,7 +3,6 @@ package fi.vm.sade.kayttooikeus.service.impl;
 import fi.vm.sade.kayttooikeus.dto.VahvaTunnistusRequestDto;
 import fi.vm.sade.kayttooikeus.dto.VahvaTunnistusResponseDto;
 import fi.vm.sade.kayttooikeus.model.Henkilo;
-import static fi.vm.sade.kayttooikeus.model.Identification.STRONG_AUTHENTICATION_IDP;
 import fi.vm.sade.kayttooikeus.model.TunnistusToken;
 import fi.vm.sade.kayttooikeus.service.IdentificationService;
 import fi.vm.sade.kayttooikeus.service.KayttajatiedotService;
@@ -32,14 +31,14 @@ public class VahvaTunnistusServiceImpl implements VahvaTunnistusService {
 
     @Override
     public VahvaTunnistusResponseDto tunnistaudu(String loginToken, VahvaTunnistusRequestDto lisatiedotDto) {
-        TunnistusToken tunnistusToken = identificationService.consumeLoginToken(loginToken);
+        TunnistusToken tunnistusToken = identificationService.getByValidLoginToken(loginToken);
         return tunnistaudu(tunnistusToken, lisatiedotDto);
     }
 
     @Override
     @Transactional(propagation = Propagation.NEVER)
     public VahvaTunnistusResponseDto tunnistauduIlmanTransaktiota(String loginToken, VahvaTunnistusRequestDto lisatiedotDto) {
-        TunnistusToken tunnistusToken = identificationService.consumeLoginToken(loginToken);
+        TunnistusToken tunnistusToken = identificationService.getByValidLoginToken(loginToken);
         return tunnistaudu(tunnistusToken, lisatiedotDto);
     }
 
@@ -57,8 +56,7 @@ public class VahvaTunnistusServiceImpl implements VahvaTunnistusService {
                 .filter(StringUtils::hasLength)
                 .ifPresent(salasana -> kayttajatiedotService.changePasswordAsAdmin(henkiloOid, salasana, LdapSynchronizationService.LdapSynchronizationType.NOW));
 
-        String authToken = identificationService.generateAuthTokenForHenkilo(henkiloOid, STRONG_AUTHENTICATION_IDP,
-                tunnistusToken.getHenkilo().getKayttajatiedot().getUsername());
+        String authToken = identificationService.consumeLoginToken(tunnistusToken.getLoginToken());
 
         return VahvaTunnistusResponseDto.builder()
                 .authToken(authToken)
