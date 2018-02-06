d7680d66adb3e443f224d78741ac31cc785e1b6a
August Kilponen <august.kilponen@gmail.com>
2018-02-06 13:19:07 +0200

    Vaihdettu virkailijan työpöydän linkki oikeaksi

--- cas-virkailija/app/util.js
+++ cas-virkailija/app/util.js
@@ -10,7 +10,7 @@ export function setCookie(name, val){
 
 export function getTargetService(){
   const service = document.body.getAttribute("data-targetService");
-  return (service && service.length > 0) ? service :  window.location.origin + '/virkailijan-stp-ui';
+  return (service && service.length > 0) ? service :  window.location.origin + '/virkailijan-tyopoyta/';
 }
 
 export function getLoginError(){
