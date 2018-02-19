b0a2ed446375ecd56f63bc0e7cb676989a3ea36d
Jarkko Pätäri <jarkko.patari@gofore.com>
2018-02-19 11:08:47 +0200

    KJHH-1308: Lisää käyttäjätunnuksen validoinnin

--- kayttooikeus-api/src/main/java/fi/vm/sade/kayttooikeus/dto/KayttajatiedotUpdateDto.java
+++ kayttooikeus-api/src/main/java/fi/vm/sade/kayttooikeus/dto/KayttajatiedotUpdateDto.java
@@ -1,16 +1,17 @@
 package fi.vm.sade.kayttooikeus.dto;
 
 import lombok.Getter;
-import lombok.NonNull;
 import lombok.Setter;
 
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 
 @Getter
 @Setter
 public class KayttajatiedotUpdateDto {
 
     @NotNull
+    @Pattern(regexp = Constants.USERNAME_REGEXP)
     private String username;
 
     public KayttajatiedotUpdateDto() {}
