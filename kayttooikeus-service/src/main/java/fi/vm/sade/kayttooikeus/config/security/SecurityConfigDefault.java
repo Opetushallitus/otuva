2a3ee0298bc4d122e7f60030d98de6413b0c46f0
Jarkko Pätäri <jarkko.patari@gofore.com>
2018-02-19 07:53:18 +0200

    KJHH-1306: Korjaa unohtunut salasana -toiminnon

--- kayttooikeus-service/src/main/java/fi/vm/sade/kayttooikeus/config/security/SecurityConfigDefault.java
+++ kayttooikeus-service/src/main/java/fi/vm/sade/kayttooikeus/config/security/SecurityConfigDefault.java
@@ -129,6 +129,7 @@ public class SecurityConfigDefault extends WebSecurityConfigurerAdapter {
                 .antMatchers("/cas/auth/token/*").permitAll()
                 .antMatchers("/cas/henkilo/*").permitAll()
                 .antMatchers("/salasana/unohtunut/*").permitAll()
+                .antMatchers("/salasana/resetointi/*").permitAll()
                 .antMatchers("/kutsu/token/*").permitAll()
                 .antMatchers("/cas/tunnistus").permitAll()
                 .antMatchers("/cas/uudelleenrekisterointi").permitAll()
