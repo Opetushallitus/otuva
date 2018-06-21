c6946cb5091499905aba797672cdf88b9fe02d1a
Tapio Jaakkola <tapio.jaakkola@github.mail.kapsi.fi>
2018-06-21 13:33:50 +0300

    KJHH-1420: Lisää varda-palvelulle tarkoitettuja oikeuksia

---
+++ kayttooikeus-service/src/main/resources/db/migration/V20180621131921000__varda_oikeudet.sql
@@ -0,0 +1,4 @@
+select insertkayttooikeus('VARDA', 'VARDA-ylläpitäjä', 'VARDA-ylläpitäjä');
+select insertkayttooikeus('VARDA', 'VARDA-pääkäyttäjä', 'VARDA-pääkäyttäjä');
+select insertkayttooikeus('VARDA', 'VARDA-katselija', 'VARDA-katselija');
+select insertkayttooikeus('VARDA', 'VARDA-tallentaja', 'VARDA-tallentaja');
