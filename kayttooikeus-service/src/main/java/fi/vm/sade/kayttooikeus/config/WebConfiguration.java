0cfd9d8c278a67cb3ce98511aba12fab4803a6d7
Petri Ryhänen <petri.ryhanen@csc.fi>
2018-07-23 15:25:08 +0300

    KJHH-1427: Update spring boot to version 2.0.3. Make sure MappingJackson2HttpMessageConverter is before StringHttpMessageConverter.

--- kayttooikeus-service/src/main/java/fi/vm/sade/kayttooikeus/config/WebConfiguration.java
+++ kayttooikeus-service/src/main/java/fi/vm/sade/kayttooikeus/config/WebConfiguration.java
@@ -3,41 +3,55 @@ package fi.vm.sade.kayttooikeus.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.http.converter.json.Jackson2ObjectMapperFactoryBean;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+import java.nio.charset.Charset;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 @Configuration
-public class WebConfiguration extends WebMvcConfigurerAdapter {
+public class WebConfiguration implements WebMvcConfigurer {
 
-    @Override
-    public void configurePathMatch(PathMatchConfigurer matcher) {
-        matcher.setUseRegisteredSuffixPatternMatch(true);
+    @Bean
+    public ObjectMapper objectMapper() {
+        return Jackson2ObjectMapperBuilder
+                .json()
+                .build()
+                .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
     }
 
-    private ObjectMapper objectMapper() {
-        Jackson2ObjectMapperFactoryBean bean = new Jackson2ObjectMapperFactoryBean();
-        bean.afterPropertiesSet();
-        ObjectMapper objectMapper = bean.getObject();
-        objectMapper.registerModule(new JavaTimeModule());
-        objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
-        return objectMapper;
-    }
-
-    private MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
+    @Bean
+    public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
         MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
         converter.setObjectMapper(objectMapper());
+        converter.setDefaultCharset(Charset.forName("UTF-8"));
         return converter;
     }
 
     @Override
-    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-        converters.add(mappingJackson2HttpMessageConverter());
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        // Add this as primary converter. Spring tests which one to use in order of this list. This needs to be
+        // before stringhttpmessageconverter or apis will return plain string as json content type string. (without
+        // double quote marks)
+        converters.add(0, mappingJackson2HttpMessageConverter());
     }
-    
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer.favorPathExtension(false)
+                .defaultContentType(MediaType.APPLICATION_JSON_UTF8);
+    }
+
 }
