a10d262ceccd9713876c47d653ed0697b3296ae2
Jarkko Pätäri <jarkko.patari@gofore.com>
2018-09-27 15:07:59 +0300

    KJHH-1453: Poistaa unohtunut salasana -toiminnon

--- kayttooikeus-service/src/main/java/fi/vm/sade/kayttooikeus/service/UnohtunutSalasanaService.java
+++
@@ -1,8 +0,0 @@
-package fi.vm.sade.kayttooikeus.service;
-
-public interface UnohtunutSalasanaService {
-
-    void lahetaPoletti(String kayttajatunnus);
-    void resetoiSalasana(String base64EncodedPoletti, String password);
-
-}
