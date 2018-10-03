82b59ca06ecd88b6d0fcab2b618f07cf5c92cd80
Jarkko Pätäri <jarkko.patari@gofore.com>
2018-10-03 10:48:55 +0300

    OH-449: Korjaa NPE jos hakukriteeriksi ei anneta käyttöoikeuksia

--- kayttooikeus-service/src/main/java/fi/vm/sade/kayttooikeus/config/mapper/KayttajaMappers.java
+++ kayttooikeus-service/src/main/java/fi/vm/sade/kayttooikeus/config/mapper/KayttajaMappers.java
@@ -2,29 +2,33 @@ package fi.vm.sade.kayttooikeus.config.mapper;
 
 import fi.vm.sade.kayttooikeus.dto.KayttajaCriteriaDto;
 import fi.vm.sade.kayttooikeus.repositories.criteria.OrganisaatioHenkiloCriteria;
-import ma.glasnost.orika.CustomMapper;
+import ma.glasnost.orika.CustomConverter;
 import ma.glasnost.orika.MappingContext;
+import ma.glasnost.orika.metadata.Type;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import static java.util.stream.Collectors.toSet;
+import java.util.Optional;
+
+import static java.util.stream.Collectors.toList;
 
 @Configuration
 public class KayttajaMappers {
 
     @Bean
-    public CustomMapper<KayttajaCriteriaDto, OrganisaatioHenkiloCriteria> kayttajaCriteriaDtoOrganisaatioHenkiloCriteriaMapper() {
-        return new CustomMapper<KayttajaCriteriaDto, OrganisaatioHenkiloCriteria>() {
+    public CustomConverter<KayttajaCriteriaDto, OrganisaatioHenkiloCriteria> kayttajaCriteriaDtoOrganisaatioHenkiloCriteriaConverter() {
+        return new CustomConverter<KayttajaCriteriaDto, OrganisaatioHenkiloCriteria>() {
             @Override
-            public void mapAtoB(KayttajaCriteriaDto a, OrganisaatioHenkiloCriteria b, MappingContext context) {
-                b.setKayttooikeudet(a.getKayttooikeudet() == null ? null : a.getKayttooikeudet().entrySet().stream()
-                        .flatMap(entry -> entry.getValue().stream().map(value -> entry.getKey() + "_" + value))
-                        .collect(toSet()));
-            }
-
-            @Override
-            public void mapBtoA(OrganisaatioHenkiloCriteria b, KayttajaCriteriaDto a, MappingContext context) {
-                throw new UnsupportedOperationException("Not implemented yet.");
+            public OrganisaatioHenkiloCriteria convert(KayttajaCriteriaDto source, Type<? extends OrganisaatioHenkiloCriteria> destinationType, MappingContext mappingContext) {
+                OrganisaatioHenkiloCriteria destination = new OrganisaatioHenkiloCriteria();
+                destination.setKayttajaTyyppi(source.getKayttajaTyyppi());
+                destination.setPassivoitu(source.getPassivoitu());
+                destination.setOrganisaatioOids(source.getOrganisaatioOids());
+                destination.setKayttooikeudet(Optional.ofNullable(source.getKayttooikeudet())
+                        .map(kayttooikeudet -> kayttooikeudet.entrySet().stream()
+                                .flatMap(entry -> entry.getValue().stream().map(value -> entry.getKey() + "_" + value))
+                                .collect(toList())).orElse(null));
+                return destination;
             }
         };
     }
