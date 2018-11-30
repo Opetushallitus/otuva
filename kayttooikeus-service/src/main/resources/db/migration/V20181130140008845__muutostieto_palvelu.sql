1e2bb01c108e33178af8eefe5740f9fc3db2e6bd
Petri Ryhänen <petri.ryhanen@csc.fi>
2018-11-30 14:11:20 +0200

    KJHH-1517: Create henkilotietomuutos service access rights.

---
+++ kayttooikeus-service/src/main/resources/db/migration/V20181130140008845__muutostieto_palvelu.sql
@@ -0,0 +1,4 @@
+select insertpalvelu('HENKILOTIETOMUUTOS', 'Henkilö muutostieto');
+
+select insertkayttooikeus('HENKILOTIETOMUUTOS', 'REKISTERINPITAJA', 'Rekisterinpitäjä');
+select insertkayttooikeus('HENKILOTIETOMUUTOS', 'PALVELUKAYTTAJA', 'Palvelukäyttäjä');
