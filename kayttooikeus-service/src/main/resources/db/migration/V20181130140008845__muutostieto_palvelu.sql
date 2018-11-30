9fa1296f93503fcab4e8f91cf1f25e8573274c62
Petri Ryhänen <petri.ryhanen@csc.fi>
2018-11-30 16:14:57 +0200

    KJHH-1517: Change name.

--- kayttooikeus-service/src/main/resources/db/migration/V20181130140008845__muutostieto_palvelu.sql
+++ kayttooikeus-service/src/main/resources/db/migration/V20181130140008845__muutostieto_palvelu.sql
@@ -1,4 +1,4 @@
-select insertpalvelu('HENKILOTIETOMUUTOS', 'Henkilö muutostieto');
+select insertpalvelu('HENKILOTIETOMUUTOS', 'Henkilötietomuutos');
 
 select insertkayttooikeus('HENKILOTIETOMUUTOS', 'REKISTERINPITAJA', 'Rekisterinpitäjä');
 select insertkayttooikeus('HENKILOTIETOMUUTOS', 'PALVELUKAYTTAJA', 'Palvelukäyttäjä');
