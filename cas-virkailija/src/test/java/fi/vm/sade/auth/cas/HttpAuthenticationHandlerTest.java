a5bf171bd889deaa289f4e95463e536d82fa9c9b
Jarkko Pätäri <jarkko.patari@gofore.com>
2019-02-06 12:21:10 +0200

    KJHH-1496: Upgrade to mockito 2.24.0
    Fixes warning "An illegal reflective access operation has occurred"

--- cas-virkailija/src/test/java/fi/vm/sade/auth/cas/HttpAuthenticationHandlerTest.java
+++ cas-virkailija/src/test/java/fi/vm/sade/auth/cas/HttpAuthenticationHandlerTest.java
@@ -19,7 +19,7 @@ import java.security.GeneralSecurityException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.catchThrowable;
-import static org.mockito.Matchers.any;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
