cdf36d5e4bad93e894a8eafbcc5818228b0da3a5
Jarkko Pätäri <jarkko.patari@gofore.com>
2019-02-12 07:53:38 +0200

    KJHH-1496: Fix saml login fallback state

--- cas-virkailija/src/main/java/fi/vm/sade/saml/action/SAMLActionConfigurer.java
+++ cas-virkailija/src/main/java/fi/vm/sade/saml/action/SAMLActionConfigurer.java
@@ -4,6 +4,7 @@ import org.apereo.cas.configuration.CasConfigurationProperties;
 import org.apereo.cas.web.flow.CasWebflowConstants;
 import org.apereo.cas.web.flow.configurer.AbstractCasWebflowConfigurer;
 import org.springframework.context.ApplicationContext;
+import org.springframework.webflow.definition.StateDefinition;
 import org.springframework.webflow.definition.registry.FlowDefinitionRegistry;
 import org.springframework.webflow.engine.ActionState;
 import org.springframework.webflow.engine.DecisionState;
@@ -29,19 +30,17 @@ public class SAMLActionConfigurer extends AbstractCasWebflowConfigurer {
     @Override
     protected void doInitialize() {
         Flow loginFlow = super.getLoginFlow();
+        StateDefinition originalStartState = loginFlow.getStartState();
 
         String decision = "requestParameters.authToken != null && !requestParameters.authToken.isEmpty()";
         DecisionState decisionState = super.createDecisionState(loginFlow, STATE_ID_SAML_DECISION, decision,
-                STATE_ID_SAML_ACTION, CasWebflowConstants.STATE_ID_VIEW_LOGIN_FORM);
+                STATE_ID_SAML_ACTION, originalStartState.getId());
         loginFlow.setStartState(decisionState);
 
         ActionState actionState = super.createActionState(loginFlow, STATE_ID_SAML_ACTION, this.samlAction);
         super.createTransitionForState(actionState, CasWebflowConstants.TRANSITION_ID_SUCCESS,
                 CasWebflowConstants.STATE_ID_CREATE_TICKET_GRANTING_TICKET);
-        super.createTransitionForState(actionState, CasWebflowConstants.TRANSITION_ID_ERROR,
-                CasWebflowConstants.STATE_ID_VIEW_LOGIN_FORM);
-        super.createTransitionForState(actionState, CasWebflowConstants.TRANSITION_ID_AUTHENTICATION_FAILURE,
-                CasWebflowConstants.STATE_ID_VIEW_LOGIN_FORM);
+        super.createStateDefaultTransition(actionState, originalStartState);
         actionState.getExitActionList().add(super.createEvaluateAction(
                 CasWebflowConstants.ACTION_ID_CLEAR_WEBFLOW_CREDENTIALS));
     }
