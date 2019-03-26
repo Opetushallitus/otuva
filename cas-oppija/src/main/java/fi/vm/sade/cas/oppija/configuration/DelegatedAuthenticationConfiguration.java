f7e21348828fdb5cf6715e5d27fefaa195575dc3
Jarkko Pätäri <jarkko.patari@gofore.com>
2019-03-26 12:17:14 +0200

    Fix delegated authentication cancel

--- cas-oppija/src/main/java/fi/vm/sade/cas/oppija/configuration/DelegatedAuthenticationConfiguration.java
+++ cas-oppija/src/main/java/fi/vm/sade/cas/oppija/configuration/DelegatedAuthenticationConfiguration.java
@@ -1,8 +1,19 @@
 package fi.vm.sade.cas.oppija.configuration;
 
+import org.apereo.cas.CentralAuthenticationService;
+import org.apereo.cas.audit.AuditableExecution;
+import org.apereo.cas.authentication.AuthenticationServiceSelectionPlan;
+import org.apereo.cas.authentication.AuthenticationSystemSupport;
+import org.apereo.cas.authentication.adaptive.AdaptiveAuthenticationPolicy;
 import org.apereo.cas.configuration.CasConfigurationProperties;
+import org.apereo.cas.services.ServicesManager;
+import org.apereo.cas.web.DelegatedClientWebflowManager;
 import org.apereo.cas.web.flow.*;
 import org.apereo.cas.web.flow.configurer.AbstractCasWebflowConfigurer;
+import org.apereo.cas.web.flow.resolver.CasDelegatingWebflowEventResolver;
+import org.apereo.cas.web.flow.resolver.CasWebflowEventResolver;
+import org.apereo.cas.web.pac4j.DelegatedSessionCookieManager;
+import org.pac4j.core.client.Clients;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ApplicationContext;
@@ -12,9 +23,12 @@ import org.springframework.core.Ordered;
 import org.springframework.webflow.definition.TransitionDefinition;
 import org.springframework.webflow.definition.registry.FlowDefinitionRegistry;
 import org.springframework.webflow.engine.ActionState;
+import org.springframework.webflow.engine.EndState;
 import org.springframework.webflow.engine.TransitionableState;
 import org.springframework.webflow.engine.builder.support.FlowBuilderServices;
 import org.springframework.webflow.execution.Action;
+import org.springframework.webflow.execution.Event;
+import org.springframework.webflow.execution.RequestContext;
 
 /**
  * This class should include only fixes to default cas delegated authentication configuration.
@@ -29,19 +43,52 @@ public class DelegatedAuthenticationConfiguration implements CasWebflowExecution
     private final Action saml2ClientLogoutAction;
     private final ApplicationContext applicationContext;
     private final CasConfigurationProperties casProperties;
+    private final CasDelegatingWebflowEventResolver initialAuthenticationAttemptWebflowEventResolver;
+    private final CasWebflowEventResolver serviceTicketRequestWebflowEventResolver;
+    private final AdaptiveAuthenticationPolicy adaptiveAuthenticationPolicy;
+    private final Clients builtClients;
+    private final ServicesManager servicesManager;
+    private final AuditableExecution registeredServiceDelegatedAuthenticationPolicyAuditableEnforcer;
+    private final DelegatedClientWebflowManager delegatedClientWebflowManager;
+    private final DelegatedSessionCookieManager delegatedSessionCookieManager;
+    private final AuthenticationSystemSupport authenticationSystemSupport;
+    private final AuthenticationServiceSelectionPlan authenticationRequestServiceSelectionStrategies;
+    private final CentralAuthenticationService centralAuthenticationService;
 
     public DelegatedAuthenticationConfiguration(FlowBuilderServices flowBuilderServices,
                                                 @Qualifier("loginFlowRegistry") FlowDefinitionRegistry loginFlowDefinitionRegistry,
                                                 @Qualifier("logoutFlowRegistry") FlowDefinitionRegistry logoutFlowDefinitionRegistry,
                                                 Action saml2ClientLogoutAction,
                                                 ApplicationContext applicationContext,
-                                                CasConfigurationProperties casProperties) {
+                                                CasConfigurationProperties casProperties,
+                                                CasDelegatingWebflowEventResolver initialAuthenticationAttemptWebflowEventResolver,
+                                                CasWebflowEventResolver serviceTicketRequestWebflowEventResolver,
+                                                AdaptiveAuthenticationPolicy adaptiveAuthenticationPolicy,
+                                                Clients builtClients,
+                                                ServicesManager servicesManager,
+                                                AuditableExecution registeredServiceDelegatedAuthenticationPolicyAuditableEnforcer,
+                                                DelegatedClientWebflowManager delegatedClientWebflowManager,
+                                                DelegatedSessionCookieManager delegatedSessionCookieManager,
+                                                AuthenticationSystemSupport authenticationSystemSupport,
+                                                AuthenticationServiceSelectionPlan authenticationRequestServiceSelectionStrategies,
+                                                CentralAuthenticationService centralAuthenticationService) {
         this.flowBuilderServices = flowBuilderServices;
         this.loginFlowDefinitionRegistry = loginFlowDefinitionRegistry;
         this.logoutFlowDefinitionRegistry = logoutFlowDefinitionRegistry;
         this.saml2ClientLogoutAction = saml2ClientLogoutAction;
         this.applicationContext = applicationContext;
         this.casProperties = casProperties;
+        this.initialAuthenticationAttemptWebflowEventResolver = initialAuthenticationAttemptWebflowEventResolver;
+        this.serviceTicketRequestWebflowEventResolver = serviceTicketRequestWebflowEventResolver;
+        this.adaptiveAuthenticationPolicy = adaptiveAuthenticationPolicy;
+        this.builtClients = builtClients;
+        this.servicesManager = servicesManager;
+        this.registeredServiceDelegatedAuthenticationPolicyAuditableEnforcer = registeredServiceDelegatedAuthenticationPolicyAuditableEnforcer;
+        this.delegatedClientWebflowManager = delegatedClientWebflowManager;
+        this.delegatedSessionCookieManager = delegatedSessionCookieManager;
+        this.authenticationSystemSupport = authenticationSystemSupport;
+        this.authenticationRequestServiceSelectionStrategies = authenticationRequestServiceSelectionStrategies;
+        this.centralAuthenticationService = centralAuthenticationService;
     }
 
     @Override
@@ -60,6 +107,11 @@ public class DelegatedAuthenticationConfiguration implements CasWebflowExecution
                 String successTargetStateId = successTransition.getTargetStateId();
                 TransitionableState state = getState(getLoginFlow(), CasWebflowConstants.ACTION_ID_DELEGATED_AUTHENTICATION);
                 createTransitionForState(state, CasWebflowConstants.TRANSITION_ID_SUCCESS, successTargetStateId, true);
+
+                // add delegatedAuthenticationAction cancel transition
+                EndState cancelState = super.createEndState(getLoginFlow(), CasWebflowConstants.TRANSITION_ID_CANCEL,
+                        '\'' + CasWebflowConfigurer.FLOW_ID_LOGOUT + '\'', true);
+                createTransitionForState(state, CasWebflowConstants.TRANSITION_ID_CANCEL, cancelState.getId());
             }
         });
     }
@@ -78,6 +130,34 @@ public class DelegatedAuthenticationConfiguration implements CasWebflowExecution
             @Override
             protected void doInitialize() {
                 // nop
+            }
+        };
+    }
+
+    // override default delegatedAuthenticationAction to automatically logout on error
+    @Bean
+    public Action delegatedAuthenticationAction() {
+        return new DelegatedClientAuthenticationAction(
+                initialAuthenticationAttemptWebflowEventResolver,
+                serviceTicketRequestWebflowEventResolver,
+                adaptiveAuthenticationPolicy,
+                builtClients,
+                servicesManager,
+                registeredServiceDelegatedAuthenticationPolicyAuditableEnforcer,
+                delegatedClientWebflowManager,
+                delegatedSessionCookieManager,
+                authenticationSystemSupport,
+                casProperties.getLocale().getParamName(),
+                casProperties.getTheme().getParamName(),
+                authenticationRequestServiceSelectionStrategies,
+                centralAuthenticationService) {
+            @Override
+            public Event doExecute(RequestContext context) {
+                try {
+                    return super.doExecute(context);
+                } catch (Exception e) {
+                    return result(CasWebflowConstants.TRANSITION_ID_CANCEL);
+                }
             }
         };
     }
