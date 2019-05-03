dd9f016f46f318630c6be0942bf79b0ac425e83d
Jarkko Pätäri <jarkko.patari@gofore.com>
2019-05-03 10:03:36 +0300

    KJHH-1627: Fix default locale

---
+++ cas-virkailija/src/main/java/fi/vm/sade/CasOphConstants.java
@@ -0,0 +1,12 @@
+package fi.vm.sade;
+
+import java.util.Set;
+
+public final class CasOphConstants {
+
+    private CasOphConstants() {
+    }
+
+    public static final Set<String> SUPPORTED_LANGUAGES = Set.of("fi", "sv");
+
+}
