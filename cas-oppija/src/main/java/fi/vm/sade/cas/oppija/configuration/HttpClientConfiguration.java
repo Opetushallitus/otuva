89f824efde417e648e6a693da4a2b936df21e4a4
Jarkko Pätäri <jarkko.patari@gofore.com>
2019-05-07 07:59:51 +0300

    Add caller-id header to cas http clients

--- cas-oppija/src/main/java/fi/vm/sade/cas/oppija/configuration/HttpClientConfiguration.java
+++ cas-oppija/src/main/java/fi/vm/sade/cas/oppija/configuration/HttpClientConfiguration.java
@@ -4,14 +4,37 @@ import fi.vm.sade.javautils.http.OphHttpClient;
 import fi.vm.sade.javautils.http.auth.CasAuthenticator;
 import fi.vm.sade.javautils.httpclient.apache.ApacheOphHttpClient;
 import fi.vm.sade.properties.OphProperties;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.message.BasicHeader;
+import org.apereo.cas.configuration.CasConfigurationProperties;
+import org.apereo.cas.configuration.model.core.authentication.HttpClientProperties;
+import org.apereo.cas.configuration.support.Beans;
+import org.apereo.cas.util.http.HttpClient;
+import org.apereo.cas.util.http.SimpleHttpClientFactoryBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+
+import javax.net.ssl.HostnameVerifier;
+
+import static java.util.Collections.singletonList;
 
 @Configuration
 public class HttpClientConfiguration {
 
     private static final String CLIENT_SUBSYSTEM_CODE = "cas-oppija";
+
+    private final CasConfigurationProperties casProperties;
+    private final SSLConnectionSocketFactory trustStoreSslSocketFactory;
+    private final HostnameVerifier hostnameVerifier;
+
+    public HttpClientConfiguration(CasConfigurationProperties casProperties,
+                                   SSLConnectionSocketFactory trustStoreSslSocketFactory,
+                                   HostnameVerifier hostnameVerifier) {
+        this.casProperties = casProperties;
+        this.trustStoreSslSocketFactory = trustStoreSslSocketFactory;
+        this.hostnameVerifier = hostnameVerifier;
+    }
 
     @Bean
     public fi.vm.sade.javautils.httpclient.OphHttpClient httpClient() {
@@ -27,6 +50,38 @@ public class HttpClientConfiguration {
                 .casServiceUrl(properties.url("oppijanumerorekisteri-service.security_check"))
                 .build();
         return new OphHttpClient.Builder(CLIENT_SUBSYSTEM_CODE).authenticator(authenticator).build();
+    }
+
+    // override cas httpclient to include caller-id header
+    @Bean
+    public HttpClient noRedirectHttpClient() {
+        return getHttpClient(false);
+    }
+
+    // override cas httpclient to include caller-id header
+    @Bean
+    public HttpClient supportsTrustStoreSslSocketFactoryHttpClient() {
+        return getHttpClient(true);
+    }
+
+    // copy from CasCoreHttpConfiguration
+    private HttpClient getHttpClient(final boolean redirectEnabled) {
+        SimpleHttpClientFactoryBean c = buildHttpClientFactoryBean();
+        c.setRedirectsEnabled(redirectEnabled);
+        c.setCircularRedirectsAllowed(redirectEnabled);
+        c.setSslSocketFactory(trustStoreSslSocketFactory);
+        c.setHostnameVerifier(hostnameVerifier);
+        return c.getObject();
+    }
+
+    // copy from CasCoreHttpConfiguration (added caller-id header)
+    private SimpleHttpClientFactoryBean buildHttpClientFactoryBean() {
+        SimpleHttpClientFactoryBean.DefaultHttpClient c = new SimpleHttpClientFactoryBean.DefaultHttpClient();
+        HttpClientProperties httpClient = casProperties.getHttpClient();
+        c.setConnectionTimeout(Beans.newDuration(httpClient.getConnectionTimeout()).toMillis());
+        c.setReadTimeout((int) Beans.newDuration(httpClient.getReadTimeout()).toMillis());
+        c.setDefaultHeaders(singletonList(new BasicHeader("Caller-Id", CLIENT_SUBSYSTEM_CODE)));
+        return c;
     }
 
 }
