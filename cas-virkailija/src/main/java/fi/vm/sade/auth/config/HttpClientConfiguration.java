3491b02693bf287c31a0da29bb5a943a0d7b4f9b
Jarkko Pätäri <jarkko.patari@gofore.com>
2019-05-07 07:25:29 +0300

    KJHH-1633: Add caller-id header to cas http clients

---
+++ cas-virkailija/src/main/java/fi/vm/sade/auth/config/HttpClientConfiguration.java
@@ -0,0 +1,68 @@
+package fi.vm.sade.auth.config;
+
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.message.BasicHeader;
+import org.apereo.cas.configuration.CasConfigurationProperties;
+import org.apereo.cas.configuration.model.core.authentication.HttpClientProperties;
+import org.apereo.cas.configuration.support.Beans;
+import org.apereo.cas.util.http.HttpClient;
+import org.apereo.cas.util.http.SimpleHttpClientFactoryBean;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.net.ssl.HostnameVerifier;
+
+import static fi.vm.sade.auth.clients.HttpClientUtil.CLIENT_SUBSYSTEM_CODE;
+import static java.util.Collections.singletonList;
+
+@Configuration
+@EnableConfigurationProperties(CasConfigurationProperties.class)
+public class HttpClientConfiguration implements BeanPostProcessor {
+
+    private final CasConfigurationProperties casProperties;
+    private final SSLConnectionSocketFactory trustStoreSslSocketFactory;
+    private final HostnameVerifier hostnameVerifier;
+
+    public HttpClientConfiguration(CasConfigurationProperties casProperties,
+                                   SSLConnectionSocketFactory trustStoreSslSocketFactory,
+                                   HostnameVerifier hostnameVerifier) {
+        this.casProperties = casProperties;
+        this.trustStoreSslSocketFactory = trustStoreSslSocketFactory;
+        this.hostnameVerifier = hostnameVerifier;
+    }
+
+    // override cas httpclient to include caller-id header
+    @Bean
+    public HttpClient noRedirectHttpClient() {
+        return getHttpClient(false);
+    }
+
+    // override cas httpclient to include caller-id header
+    @Bean
+    public HttpClient supportsTrustStoreSslSocketFactoryHttpClient() {
+        return getHttpClient(true);
+    }
+
+    // copy from CasCoreHttpConfiguration
+    private HttpClient getHttpClient(final boolean redirectEnabled) {
+        SimpleHttpClientFactoryBean c = buildHttpClientFactoryBean();
+        c.setRedirectsEnabled(redirectEnabled);
+        c.setCircularRedirectsAllowed(redirectEnabled);
+        c.setSslSocketFactory(trustStoreSslSocketFactory);
+        c.setHostnameVerifier(hostnameVerifier);
+        return c.getObject();
+    }
+
+    // copy from CasCoreHttpConfiguration (added caller-id header)
+    private SimpleHttpClientFactoryBean buildHttpClientFactoryBean() {
+        SimpleHttpClientFactoryBean.DefaultHttpClient c = new SimpleHttpClientFactoryBean.DefaultHttpClient();
+        HttpClientProperties httpClient = casProperties.getHttpClient();
+        c.setConnectionTimeout(Beans.newDuration(httpClient.getConnectionTimeout()).toMillis());
+        c.setReadTimeout((int) Beans.newDuration(httpClient.getReadTimeout()).toMillis());
+        c.setDefaultHeaders(singletonList(new BasicHeader("Caller-Id", CLIENT_SUBSYSTEM_CODE)));
+        return c;
+    }
+
+}
