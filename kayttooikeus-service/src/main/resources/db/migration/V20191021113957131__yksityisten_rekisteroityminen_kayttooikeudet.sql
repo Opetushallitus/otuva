871af2c612e44a6e8bebb92ff97d68297292f172
Jani Kaarela <jani.kaarela@gmail.com>
2019-10-21 16:08:13 +0300

    Yhdistetty oikeuksia.
    Yksityisten rekisteröitymisten oikeudet yhdistetty
    yhdeksi CRUD-oikeudeksi, oikeuden nimeä lyhennetty.

--- kayttooikeus-service/src/main/resources/db/migration/V20191021113957131__yksityisten_rekisteroityminen_kayttooikeudet.sql
+++ kayttooikeus-service/src/main/resources/db/migration/V20191021113957131__yksityisten_rekisteroityminen_kayttooikeudet.sql
@@ -1,4 +1,2 @@
 SELECT insertpalvelu('YKSITYISTEN_REKISTEROITYMINEN', 'Yksityisten rekisteröityminen -palvelu');
-SELECT insertkayttooikeus('YKSITYISTEN_REKISTEROITYMINEN', 'YKSITYISTEN_REKISTEROITYMINEN_READ', 'Rekisteröitymisten lukuoikeus');
-SELECT insertkayttooikeus('YKSITYISTEN_REKISTEROITYMINEN', 'YKSITYISTEN_REKISTEROITYMINEN_UPDATE', 'Rekisteröitymisten muokkausoikeus');
-SELECT insertkayttooikeus('YKSITYISTEN_REKISTEROITYMINEN', 'YKSITYISTEN_REKISTEROITYMINEN_DELETE', 'Rekisteröitymisten poisto-oikeus');
+SELECT insertkayttooikeus('YKSITYISTEN_REKISTEROITYMINEN', 'CRUD', 'Rekisteröitymisten luonti-, luku-, muokkaus- ja poisto-oikeus');
