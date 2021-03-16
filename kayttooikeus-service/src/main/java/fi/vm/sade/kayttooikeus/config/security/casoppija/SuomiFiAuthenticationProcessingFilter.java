72897ef838dd755d6b7e40ba5b7c23b957d5b97c
Jani Kaarela <jani.kaarela@gmail.com>
2021-03-16 12:33:12 +0200

    Siistimistä, testejä.

--- kayttooikeus-service/src/main/java/fi/vm/sade/kayttooikeus/config/security/casoppija/SuomiFiAuthenticationProcessingFilter.java
+++ kayttooikeus-service/src/main/java/fi/vm/sade/kayttooikeus/config/security/casoppija/SuomiFiAuthenticationProcessingFilter.java
@@ -14,10 +14,10 @@ import java.util.stream.Stream;
 
 public class SuomiFiAuthenticationProcessingFilter extends AbstractPreAuthenticatedProcessingFilter {
 
-    public static final String OPPIJA_TICKET_PARAM_NAME = "ticket";
     public static final String HETU_ATTRIBUTE = "nationalIdentificationNumber";
     public static final String SUKUNIMI_ATTRIBUTE = "sn";
     public static final String ETUNIMET_ATTRIBUTE = "firstName";
+    static final String OPPIJA_TICKET_PARAM_NAME = "ticket";
     static final String SUOMI_FI_DETAILS_ATTR_KEY = "suomiFiDetails";
 
     private static final Logger LOGGER = LoggerFactory.getLogger(SuomiFiAuthenticationProcessingFilter.class);
@@ -30,27 +30,18 @@ public class SuomiFiAuthenticationProcessingFilter extends AbstractPreAuthentica
     @Override
     protected Object getPreAuthenticatedPrincipal(HttpServletRequest request) {
         String ticket = request.getParameter(OPPIJA_TICKET_PARAM_NAME);
-        String parameters = request.getParameterMap().entrySet().stream()
-                .filter(entry -> !entry.getKey().equals(OPPIJA_TICKET_PARAM_NAME))
-                .map(entry ->
-                        Stream.of(entry.getValue())
-                                .map(value -> entry.getKey() + "=" + value)
-                                .collect(Collectors.joining("&")))
-                .collect(Collectors.joining("&"));
-        String service = String.join("?", request.getRequestURL(), parameters);
         if (ticket != null) {
+            String parameters = extractServiceParameters(request);
+            String service = String.join("?", request.getRequestURL(), parameters);
             LOGGER.info("Validating ticket: \"{}\"", ticket);
             try {
                 Assertion assertion = ticketValidator.validate(ticket, service);
                 if (assertion.isValid()) {
                     LOGGER.info("Ticket \"{}\" is valid.", ticket);
-                    Map<String, Object> attributes = assertion.getPrincipal().getAttributes();
-                    String hetu = (String) attributes.get(HETU_ATTRIBUTE);
-                    String sukunimi = (String) attributes.get(SUKUNIMI_ATTRIBUTE);
-                    String etunimet = (String) attributes.get(ETUNIMET_ATTRIBUTE);
-                    SuomiFiUserDetails details = new SuomiFiUserDetails(hetu, sukunimi, etunimet);
+                    SuomiFiUserDetails details = extractUserDetails(assertion);
+                    // asetetaan requestiin, SuomiFiAuthenticationDetailsSourcen saataville
                     request.setAttribute(SUOMI_FI_DETAILS_ATTR_KEY, details);
-                    return String.join(", ", sukunimi, etunimet);
+                    return String.join(", ", details.sukunimi, details.etunimet);
                 } else {
                     LOGGER.warn("Invalid ticket: \"{}\"", ticket);
                 }
@@ -59,6 +50,24 @@ public class SuomiFiAuthenticationProcessingFilter extends AbstractPreAuthentica
             }
         }
         return null;
+    }
+
+    String extractServiceParameters(HttpServletRequest request) {
+        return request.getParameterMap().entrySet().stream()
+                .filter(entry -> !entry.getKey().equals(OPPIJA_TICKET_PARAM_NAME))
+                .map(entry ->
+                        Stream.of(entry.getValue())
+                                .map(value -> entry.getKey() + "=" + value)
+                                .collect(Collectors.joining("&")))
+                .collect(Collectors.joining("&"));
+    }
+
+    SuomiFiUserDetails extractUserDetails(Assertion assertion) {
+        Map<String, Object> attributes = assertion.getPrincipal().getAttributes();
+        String hetu = (String) attributes.get(HETU_ATTRIBUTE);
+        String sukunimi = (String) attributes.get(SUKUNIMI_ATTRIBUTE);
+        String etunimet = (String) attributes.get(ETUNIMET_ATTRIBUTE);
+        return new SuomiFiUserDetails(hetu, sukunimi, etunimet);
     }
 
     @Override
