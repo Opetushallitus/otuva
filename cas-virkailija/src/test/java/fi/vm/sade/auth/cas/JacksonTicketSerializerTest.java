1b0d5408ddd1d48ef0ff32fc58d983e2a5f92896
Teemu Kurki <teemukin65@gmail.com>
2022-04-05 14:58:39 +0300

    Revert "OK-253 JacksonTicketSerializerTest proxyTicket & proxyGrantingTicket dropped for now to get this compile"
    This reverts commit 368ec1fedcc24a12d67568e4e6eb10cc3c228e91.

--- cas-virkailija/src/test/java/fi/vm/sade/auth/cas/JacksonTicketSerializerTest.java
+++ cas-virkailija/src/test/java/fi/vm/sade/auth/cas/JacksonTicketSerializerTest.java
@@ -4,10 +4,14 @@ import org.apereo.cas.authentication.DefaultAuthentication;
 import org.apereo.cas.authentication.principal.NullPrincipal;
 import org.apereo.cas.authentication.principal.Service;
 import org.apereo.cas.authentication.principal.WebApplicationServiceFactory;
+import org.apereo.cas.services.*;
 import org.apereo.cas.ticket.*;
 import org.apereo.cas.ticket.expiration.NeverExpiresExpirationPolicy;
-import org.apereo.cas.ticket.factory.DefaultTicketGrantingTicketFactory;
-import org.apereo.cas.ticket.factory.DefaultTransientSessionTicketFactory;
+import org.apereo.cas.ticket.factory.*;
+import org.apereo.cas.ticket.proxy.ProxyGrantingTicket;
+import org.apereo.cas.ticket.proxy.ProxyGrantingTicketFactory;
+import org.apereo.cas.ticket.proxy.ProxyTicket;
+import org.apereo.cas.ticket.proxy.ProxyTicketFactory;
 import org.apereo.cas.util.DefaultUniqueTicketIdGenerator;
 import org.apereo.cas.util.crypto.CipherExecutor;
 import org.junit.Before;
@@ -16,14 +20,13 @@ import org.springframework.context.ApplicationEventPublisher;
 
 import java.time.ZonedDateTime;
 
-import static java.util.Collections.emptyList;
-import static java.util.Collections.emptyMap;
+import static java.util.Collections.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class JacksonTicketSerializerTest {
 
     private TicketSerializer ticketSerializer;
-    private Service testService;
+    private ServicesManager servicesManager;
 
     @Before
     public void setup() {
@@ -31,7 +34,8 @@ public class JacksonTicketSerializerTest {
 
         ApplicationEventPublisher fakeEventPublisher = event -> {
         };
-        testService = new WebApplicationServiceFactory().createService("service123");
+        ServiceRegistry serviceRegistry = new ImmutableInMemoryServiceRegistry(emptyList(),fakeEventPublisher,emptySet());
+        servicesManager = new DefaultServicesManager(serviceRegistry,fakeEventPublisher, emptySet());
     }
 
     @Test
@@ -53,10 +57,60 @@ public class JacksonTicketSerializerTest {
         ticketGrantingTicket.markTicketExpired();
         String expiredTicketGrantingTicketAsJson = ticketSerializer.toJson(ticketGrantingTicket);
 
+        // service ticket
+        ServiceTicketFactory ticketFactory = new DefaultServiceTicketFactory(
+                getNeverExpiringServiceTicketExpirationPolicyBuilder(),
+                emptyMap(),
+                true,
+                CipherExecutor.noOpOfStringToString(),
+                servicesManager
+        );
+        Service service = new WebApplicationServiceFactory().createService("service123");
+        ServiceTicket serviceTicket = ticketFactory.create((TicketGrantingTicket) ticketGrantingTicketFromJson,
+                service, true, ServiceTicket.class);
+
+        String serviceTicketAsJson = ticketSerializer.toJson(serviceTicket);
+        System.out.println(serviceTicketAsJson);
+        Ticket serviceTicketFromJson = ticketSerializer.fromJson(serviceTicketAsJson,
+                expiredTicketGrantingTicketAsJson);
+        assertThat(serviceTicketFromJson).isInstanceOf(ServiceTicket.class).isEqualByComparingTo(serviceTicket).returns(true, ticket -> ticket.getTicketGrantingTicket().isExpired());
+
+        // proxy granting ticket
+        ProxyGrantingTicketFactory proxyGrantingTicketFactory =
+                new DefaultProxyGrantingTicketFactory(new DefaultUniqueTicketIdGenerator(),
+                        getNeverExpiringProxyGrantingTicketExpirationPolicyBuilder(),
+                        CipherExecutor.noOpOfStringToString());
+        ProxyGrantingTicket proxyGrantingTicket =
+                proxyGrantingTicketFactory.create((ServiceTicket) serviceTicketFromJson,
+                        new DefaultAuthentication(ZonedDateTime.now(), new NullPrincipal(), emptyMap(), emptyMap(),
+                                emptyList()), ProxyGrantingTicket.class);
+
+        String proxyGrantingTicketAsJson = ticketSerializer.toJson(proxyGrantingTicket);
+        System.out.println(proxyGrantingTicketAsJson);
+        Ticket proxyGrantingTicketFromJson = ticketSerializer.fromJson(proxyGrantingTicketAsJson,
+                expiredTicketGrantingTicketAsJson);
+        assertThat(proxyGrantingTicketFromJson).isInstanceOf(ProxyGrantingTicket.class).isEqualByComparingTo(proxyGrantingTicket).returns(true, ticket -> ticket.getTicketGrantingTicket().isExpired());
+
+        // proxy ticket
+        ProxyTicketFactory proxyTicketFactory = new DefaultProxyTicketFactory(
+                getNeverExpiringProxyTicketExpirationPolicyBuilder(),
+                emptyMap(),
+                CipherExecutor.noOpOfStringToString(),
+                true,
+                servicesManager
+        );
+        ProxyTicket proxyTicket = proxyTicketFactory.create((ProxyGrantingTicket) proxyGrantingTicketFromJson,
+                service, ProxyTicket.class);
+
+        String proxyTicketAsJson = ticketSerializer.toJson(proxyTicket);
+        System.out.println(proxyTicketAsJson);
+        Ticket proxyTicketFromJson = ticketSerializer.fromJson(proxyTicketAsJson, expiredTicketGrantingTicketAsJson);
+        assertThat(proxyTicketFromJson).isInstanceOf(ProxyTicket.class).isEqualByComparingTo(proxyTicket).returns(true, ticket -> ticket.getTicketGrantingTicket().isExpired());
+
         // transient session ticket
         TransientSessionTicketFactory transientSessionTicketFactory =
                 new DefaultTransientSessionTicketFactory(getNeverExpiringTransientSessionTicketExpirationPolicyBuilder());
-        TransientSessionTicket transientSessionTicket = transientSessionTicketFactory.create(testService);
+        TransientSessionTicket transientSessionTicket = transientSessionTicketFactory.create(service);
 
         String transientSessionTicketAsJson = ticketSerializer.toJson(transientSessionTicket);
         System.out.println(transientSessionTicketAsJson);
@@ -78,6 +132,35 @@ public class JacksonTicketSerializerTest {
         };
     }
 
+
+    private ExpirationPolicyBuilder<ProxyGrantingTicket> getNeverExpiringProxyGrantingTicketExpirationPolicyBuilder() {
+        return new ExpirationPolicyBuilder<>() {
+            @Override
+            public ExpirationPolicy buildTicketExpirationPolicy() {
+                return NeverExpiresExpirationPolicy.INSTANCE;
+            }
+
+            @Override
+            public Class<ProxyGrantingTicket> getTicketType() {
+                return ProxyGrantingTicket.class;
+            }
+        };
+    }
+
+    private ExpirationPolicyBuilder<ProxyTicket> getNeverExpiringProxyTicketExpirationPolicyBuilder() {
+        return new ExpirationPolicyBuilder<>() {
+            @Override
+            public ExpirationPolicy buildTicketExpirationPolicy() {
+                return NeverExpiresExpirationPolicy.INSTANCE;
+            }
+
+            @Override
+            public Class<ProxyTicket> getTicketType() {
+                return ProxyTicket.class;
+            }
+        };
+    }
+
     private ExpirationPolicyBuilder<TransientSessionTicket> getNeverExpiringTransientSessionTicketExpirationPolicyBuilder() {
         return new ExpirationPolicyBuilder<>() {
             @Override
@@ -91,4 +174,19 @@ public class JacksonTicketSerializerTest {
             }
         };
     }
+
+    private ExpirationPolicyBuilder<ServiceTicket> getNeverExpiringServiceTicketExpirationPolicyBuilder() {
+        return new ExpirationPolicyBuilder<>() {
+            @Override
+            public ExpirationPolicy buildTicketExpirationPolicy() {
+                return NeverExpiresExpirationPolicy.INSTANCE;
+            }
+
+            @Override
+            public Class<ServiceTicket> getTicketType() {
+                return ServiceTicket.class;
+            }
+        };
+    }
+
 }
