6ee3609a692ae9db366eeed608f2918355d8ac4a
Teemu Kurki <teemukin65@gmail.com>
2022-04-22 17:16:19 +0300

    OK-256 CAS-provided ticket cleaner replaces homebrewed one

---
+++ cas-virkailija/src/main/resources/db/migration/V20220422170801__cas_tickets.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS cas_tickets
+(
+    id            VARCHAR(768)   NOT NULL
+        PRIMARY KEY,
+    body          VARCHAR(32000) NOT NULL,
+    creation_time TIMESTAMP      NOT NULL,
+    parent_id     VARCHAR(1024),
+    principal_id  VARCHAR(1024),
+    type          VARCHAR(1024)  NOT NULL
+);
+
+ALTER TABLE cas_tickets
+    OWNER TO cas;
