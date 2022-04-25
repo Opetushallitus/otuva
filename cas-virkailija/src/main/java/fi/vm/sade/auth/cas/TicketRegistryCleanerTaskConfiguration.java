f7a8aa0a50f4f10a1597151f67267e50c8c22fd7
Teemu Kurki <teemukin65@gmail.com>
2022-04-25 16:19:05 +0300

    OK-256 Proper RegistryCleaner added.

--- cas-virkailija/src/main/java/fi/vm/sade/auth/cas/TicketRegistryCleanerTaskConfiguration.java
+++ cas-virkailija/src/main/java/fi/vm/sade/auth/cas/TicketRegistryCleanerTaskConfiguration.java
@@ -4,7 +4,12 @@ import com.github.kagkarlsson.scheduler.task.Task;
 import com.github.kagkarlsson.scheduler.task.helper.Tasks;
 import com.github.kagkarlsson.scheduler.task.schedule.FixedDelay;
 import org.apereo.cas.configuration.CasConfigurationProperties;
+import org.apereo.cas.logout.LogoutManager;
+import org.apereo.cas.ticket.registry.DefaultTicketRegistryCleaner;
+import org.apereo.cas.ticket.registry.NoOpLockingStrategy;
+import org.apereo.cas.ticket.registry.TicketRegistry;
 import org.apereo.cas.ticket.registry.TicketRegistryCleaner;
+import org.apereo.cas.ticket.registry.support.LockingStrategy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,6 +17,13 @@ import java.time.Duration;
 
 @Configuration
 public class TicketRegistryCleanerTaskConfiguration {
+
+    @Bean
+    public TicketRegistryCleaner ticketRegistryCleaner(LogoutManager logoutManager, TicketRegistry ticketRegistry) {
+        // If this causes problems, consider using JpaLockingStrategy ..
+        LockingStrategy lockingStrategy = new NoOpLockingStrategy();
+        return new DefaultTicketRegistryCleaner( lockingStrategy,  logoutManager,  ticketRegistry);
+    }
 
     @Bean
     public Task<Void> ticketRegistryCleanerTask(CasConfigurationProperties casProperties, TicketRegistryCleaner ticketRegistryCleaner) {
