c5f7d1aa8d48e5585428acc7ce65c81c34ebacae
Teemu Kurki <teemukin65@gmail.com>
2022-04-27 12:16:50 +0300

    OK-262 reverted to vanilla DefaultLockRepository, INT_ prefix for lock tables

--- cas-virkailija/src/main/java/fi/vm/sade/auth/cas/TicketRegistryCleanerTaskConfiguration.java
+++ cas-virkailija/src/main/java/fi/vm/sade/auth/cas/TicketRegistryCleanerTaskConfiguration.java
@@ -5,15 +5,16 @@ import com.github.kagkarlsson.scheduler.task.Task;
 import com.github.kagkarlsson.scheduler.task.helper.Tasks;
 import com.github.kagkarlsson.scheduler.task.schedule.FixedDelay;
 import org.apereo.cas.configuration.CasConfigurationProperties;
-
 import org.apereo.cas.logout.LogoutManager;
 import org.apereo.cas.ticket.registry.DefaultTicketRegistryCleaner;
 import org.apereo.cas.ticket.registry.TicketRegistry;
 import org.apereo.cas.ticket.registry.TicketRegistryCleaner;
 import org.apereo.cas.util.lock.DefaultLockRepository;
 import org.apereo.cas.util.lock.LockRepository;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.integration.jdbc.lock.JdbcLockRegistry;
 import org.springframework.integration.support.locks.LockRegistry;
 
@@ -24,17 +25,10 @@ import java.time.Duration;
 public class TicketRegistryCleanerTaskConfiguration {
 
     @Bean
-    public TicketRegistryCleaner ticketRegistryCleaner(LogoutManager logoutManager, TicketRegistry ticketRegistry, DataSource dataSource) {
-        org.springframework.integration.jdbc.lock.LockRepository repository = new org.springframework.integration.jdbc.lock.DefaultLockRepository(dataSource);
-        LockRegistry lockRegistry = new JdbcLockRegistry(repository);
-        LockRepository lockRepository = new DefaultLockRepository(lockRegistry);
-
-        return new DefaultTicketRegistryCleaner( lockRepository,  logoutManager,  ticketRegistry);
-    }
-
-    @Bean
-    public Task<Void> ticketRegistryCleanerTask(CasConfigurationProperties casProperties, TicketRegistryCleaner ticketRegistryCleaner) {
-        Duration duration = Duration.parse(casProperties.getTicket().getRegistry().getCleaner().getSchedule().getRepeatInterval());
+    public Task<Void> ticketRegistryCleanerTask(CasConfigurationProperties casProperties,
+                                                TicketRegistryCleaner ticketRegistryCleaner) {
+        Duration duration =
+                Duration.parse(casProperties.getTicket().getRegistry().getCleaner().getSchedule().getRepeatInterval());
         return Tasks.recurring("cas-ticket-registry-cleaner", FixedDelay.of(duration))
                 .execute((taskInstance, executionContext) -> ticketRegistryCleaner.clean());
     }
