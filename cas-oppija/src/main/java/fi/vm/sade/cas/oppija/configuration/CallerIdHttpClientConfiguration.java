67b0f4c3e6d92358de0315de70a52b6b9a396071
Teemu Kurki <teemukin65@gmail.com>
2022-05-09 15:50:55 +0300

    OK-260 CallerIdHttpClientConfiguration renewed

--- cas-oppija/src/main/java/fi/vm/sade/cas/oppija/configuration/CallerIdHttpClientConfiguration.java
+++ cas-oppija/src/main/java/fi/vm/sade/cas/oppija/configuration/CallerIdHttpClientConfiguration.java
@@ -4,39 +4,50 @@ import fi.vm.sade.javautils.http.OphHttpClient;
 import fi.vm.sade.javautils.http.auth.CasAuthenticator;
 import fi.vm.sade.javautils.httpclient.apache.ApacheOphHttpClient;
 import fi.vm.sade.properties.OphProperties;
+import lombok.val;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.http.Header;
+import org.apache.http.HttpHost;
 import org.apache.http.client.CookieStore;
-import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.socket.LayeredConnectionSocketFactory;
 import org.apache.http.impl.NoConnectionReuseStrategy;
 import org.apache.http.impl.client.BasicCookieStore;
 import org.apache.http.impl.cookie.BasicClientCookie;
 import org.apache.http.message.BasicHeader;
+import org.apereo.cas.authentication.CasSSLContext;
 import org.apereo.cas.configuration.CasConfigurationProperties;
 import org.apereo.cas.configuration.model.core.authentication.HttpClientProperties;
 import org.apereo.cas.configuration.support.Beans;
 import org.apereo.cas.util.http.HttpClient;
 import org.apereo.cas.util.http.SimpleHttpClientFactoryBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 
 import javax.net.ssl.HostnameVerifier;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 @Configuration
-public class HttpClientConfiguration {
+@EnableConfigurationProperties(CasConfigurationProperties.class)
+public class CallerIdHttpClientConfiguration {
 
     private static final String CALLER_ID = "1.2.246.562.10.00000000001.cas-oppija";
 
     private final CasConfigurationProperties casProperties;
-    private final SSLConnectionSocketFactory trustStoreSslSocketFactory;
+    private final CasSSLContext casSslContext;
     private final HostnameVerifier hostnameVerifier;
+    private final LayeredConnectionSocketFactory trustStoreSslSocketFactory;
 
-    public HttpClientConfiguration(CasConfigurationProperties casProperties,
-                                   SSLConnectionSocketFactory trustStoreSslSocketFactory,
-                                   HostnameVerifier hostnameVerifier) {
+    public CallerIdHttpClientConfiguration(CasConfigurationProperties casProperties,
+                                           CasSSLContext casSslContext,
+                                           HostnameVerifier hostnameVerifier,
+                                           LayeredConnectionSocketFactory trustStoreSslSocketFactory) {
         this.casProperties = casProperties;
-        this.trustStoreSslSocketFactory = trustStoreSslSocketFactory;
+        this.casSslContext = casSslContext;
         this.hostnameVerifier = hostnameVerifier;
+        this.trustStoreSslSocketFactory = trustStoreSslSocketFactory;
     }
 
     @Bean
@@ -72,8 +83,6 @@ public class HttpClientConfiguration {
         SimpleHttpClientFactoryBean c = buildHttpClientFactoryBean();
         c.setRedirectsEnabled(redirectEnabled);
         c.setCircularRedirectsAllowed(redirectEnabled);
-        c.setSslSocketFactory(trustStoreSslSocketFactory);
-        c.setHostnameVerifier(hostnameVerifier);
         return c.getObject();
     }
 
@@ -83,12 +92,19 @@ public class HttpClientConfiguration {
         HttpClientProperties httpClient = casProperties.getHttpClient();
         c.setConnectionTimeout(Beans.newDuration(httpClient.getConnectionTimeout()).toMillis());
         c.setReadTimeout((int) Beans.newDuration(httpClient.getReadTimeout()).toMillis());
-        c.setDefaultHeaders(
-                Arrays.asList(
-                        new BasicHeader("Caller-Id", CALLER_ID),
-                        new BasicHeader("CSRF", CALLER_ID)
-                )
-        );
+
+        if (StringUtils.isNotBlank(httpClient.getProxyHost()) && httpClient.getProxyPort() > 0) {
+            c.setProxy(new HttpHost(httpClient.getProxyHost(), httpClient.getProxyPort()));
+        }
+        c.setSslSocketFactory(trustStoreSslSocketFactory);
+        c.setHostnameVerifier(hostnameVerifier);
+        c.setSslContext(casSslContext.getSslContext());
+        c.setTrustManagers(casSslContext.getTrustManagers());
+        val defaultHeaders = new ArrayList<Header>();
+        httpClient.getDefaultHeaders().forEach((name, value) -> defaultHeaders.add(new BasicHeader(name, value)));
+        defaultHeaders.add(new BasicHeader("Caller-Id", CALLER_ID));
+        defaultHeaders.add(new BasicHeader("CSRF", CALLER_ID));
+        c.setDefaultHeaders(defaultHeaders);
         CookieStore cookieStore = new BasicCookieStore();
         cookieStore.addCookie(new BasicClientCookie("CSRF", CALLER_ID));
         c.setCookieStore(cookieStore);
