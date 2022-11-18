84d1a887529f73a1dda51158ae7508c13997b848
Henry Heikkinen <henry.heikkinen@reaktor.com>
2022-11-18 13:14:48 +0200

    Remove unused redisAuditConnectionFactory bean
    Since the audit mechanism is not in use there is onlyy single bean
    providing RedisConnectionFactory and therefore the custom bean tagged as
    @Primary is not needed.

--- cas-virkailija/src/main/java/fi/vm/sade/auth/config/RedisConfig.java
+++ cas-virkailija/src/main/java/fi/vm/sade/auth/config/RedisConfig.java
@@ -1,13 +1,6 @@
 package fi.vm.sade.auth.config;
 
-import lombok.val;
-import org.apereo.cas.authentication.CasSSLContext;
-import org.apereo.cas.configuration.CasConfigurationProperties;
-import org.apereo.cas.redis.core.RedisObjectFactory;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.session.data.redis.config.ConfigureRedisAction;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
@@ -17,14 +10,5 @@ public class RedisConfig {
     @Bean
     public static ConfigureRedisAction configureRedisAction() {
         return ConfigureRedisAction.NO_OP;
-    }
-
-    @Primary
-    @Bean
-    public RedisConnectionFactory redisAuditConnectionFactory(
-            @Qualifier(CasSSLContext.BEAN_NAME) final CasSSLContext casSslContext,
-            final CasConfigurationProperties casProperties) {
-        val redis = casProperties.getAudit().getRedis();
-        return RedisObjectFactory.newRedisConnectionFactory(redis, casSslContext);
     }
 }
