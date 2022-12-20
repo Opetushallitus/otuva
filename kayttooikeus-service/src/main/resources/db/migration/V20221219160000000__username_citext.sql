0e336f8b0c2f1a4aafcca488c5090277ff6f3af8
Tomi Karlstedt <tomi.karlstedt@reaktor.com>
2022-12-20 09:22:59 +0200

    KJHH-2202 update usernames to not include case insensitive duplicates before altering type to citext

--- kayttooikeus-service/src/main/resources/db/migration/V20221219160000000__username_citext.sql
+++ kayttooikeus-service/src/main/resources/db/migration/V20221219160000000__username_citext.sql
@@ -1,3 +1,8 @@
 CREATE EXTENSION IF NOT EXISTS citext;
 
+UPDATE kayttajatiedot
+SET username = CONCAT(username, CAST(id as varchar))
+WHERE username IS NOT NULL
+  AND id NOT IN (SELECT min(id) FROM kayttajatiedot GROUP BY LOWER(username));
+
 ALTER TABLE kayttajatiedot ALTER COLUMN username TYPE citext;
