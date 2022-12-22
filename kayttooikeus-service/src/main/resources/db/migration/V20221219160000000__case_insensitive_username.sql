4a6622a68addd17165122e4097295c102493d40a
Tomi Karlstedt <tomi.karlstedt@reaktor.com>
2022-12-22 14:14:20 +0200

    KJHH-2202 use create index syntax

--- kayttooikeus-service/src/main/resources/db/migration/V20221219160000000__case_insensitive_username.sql
+++ kayttooikeus-service/src/main/resources/db/migration/V20221219160000000__case_insensitive_username.sql
@@ -3,5 +3,4 @@ SET username = CONCAT(username, CAST(id as varchar))
 WHERE username IS NOT NULL
   AND id NOT IN (SELECT min(id) FROM kayttajatiedot GROUP BY LOWER(username));
 
-ALTER TABLE kayttajatiedot
-ADD CONSTRAINT username_ci_unique UNIQUE (lower(username));
+CREATE UNIQUE INDEX username_ci_unique ON kayttajatiedot (lower(username));
