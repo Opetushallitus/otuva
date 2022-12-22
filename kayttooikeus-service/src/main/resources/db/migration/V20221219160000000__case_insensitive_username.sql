b345c616e65cb780bd7b9953153b5fb5d654f5d3
Tomi Karlstedt <tomi.karlstedt@reaktor.com>
2022-12-22 14:07:00 +0200

    KJHH-2202 use lower case index instead of collation. basic string operations such as LIKE, regular expressions or substring operations do not work with the non-deterministic collations

--- kayttooikeus-service/src/main/resources/db/migration/V20221219160000000__case_insensitive_username.sql
+++ kayttooikeus-service/src/main/resources/db/migration/V20221219160000000__case_insensitive_username.sql
@@ -3,7 +3,5 @@ SET username = CONCAT(username, CAST(id as varchar))
 WHERE username IS NOT NULL
   AND id NOT IN (SELECT min(id) FROM kayttajatiedot GROUP BY LOWER(username));
 
-CREATE COLLATION case_insensitive (provider = icu, locale = 'und-u-ks-level2', deterministic = false);
-
 ALTER TABLE kayttajatiedot
-ALTER COLUMN username SET DATA TYPE character varying(255) COLLATE "case_insensitive";
+ADD CONSTRAINT username_ci_unique UNIQUE (lower(username));
