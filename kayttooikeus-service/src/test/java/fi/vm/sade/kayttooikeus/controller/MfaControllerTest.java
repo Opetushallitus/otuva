08602e61ff92461a03e23368b923fe3372dae624
Tomi Karlstedt <tomi.karlstedt@reaktor.com>
2023-01-12 17:30:27 +0200

    KJHH-2256 remove unused imports

--- kayttooikeus-service/src/test/java/fi/vm/sade/kayttooikeus/controller/MfaControllerTest.java
+++ kayttooikeus-service/src/test/java/fi/vm/sade/kayttooikeus/controller/MfaControllerTest.java
@@ -1,10 +1,8 @@
 package fi.vm.sade.kayttooikeus.controller;
 
-import fi.vm.sade.kayttooikeus.config.properties.CasProperties;
 import fi.vm.sade.kayttooikeus.service.KayttajatiedotService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
