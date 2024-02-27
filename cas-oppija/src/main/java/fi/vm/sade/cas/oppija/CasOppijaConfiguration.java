8d6d21aec65052f3f8e397ac2a731223cddfd5cc
Henry Heikkinen <henry.heikkinen@reaktor.com>
2024-02-27 12:53:25 +0200

    KJHH-2483 Serialize TransientSessionTickets using Java Serialization API instead of Jackson
    Some of the nested objects can't be serialized with Jackson

--- cas-oppija/src/main/java/fi/vm/sade/cas/oppija/CasOppijaConfiguration.java
+++ cas-oppija/src/main/java/fi/vm/sade/cas/oppija/CasOppijaConfiguration.java
@@ -1,7 +1,18 @@
 package fi.vm.sade.cas.oppija;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apereo.cas.ticket.serialization.TicketSerializationExecutionPlanConfigurer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
+@Slf4j
 @ComponentScan("fi.vm.sade.cas.oppija")
 public class CasOppijaConfiguration {
+    @Bean
+    public TicketSerializationExecutionPlanConfigurer ticketSerializationExecutionPlanConfigurer() {
+        LOGGER.info("Initializing ticketSerializationExecutionPlanConfigurer");
+        return plan -> {
+            plan.registerTicketSerializer(new CasOppijaTransientSessionTicketSerializer());
+        };
+    }
 }
