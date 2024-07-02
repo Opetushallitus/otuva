965c6f91fb76daf89523f86a39b88ae29b6110d2
Henry Heikkinen <henry.heikkinen@reaktor.com>
2024-07-02 14:44:58 +0300

    OPHYK-35 Create HostedZone otuva

--- infra/src/cdk-app.ts
+++ infra/src/cdk-app.ts
@@ -1,8 +1,10 @@
 import * as cdk from "aws-cdk-lib";
+import { aws_sns } from "aws-cdk-lib";
 import * as constructs from "constructs";
+import * as route53 from "aws-cdk-lib/aws-route53";
+import * as ssm from "aws-cdk-lib/aws-ssm";
 
 import { ALARM_TOPIC_ARN, prefix, QUALIFIER } from "./shared-account";
-import { aws_sns } from "aws-cdk-lib";
 
 class CdkApp extends cdk.App {
   constructor(props: cdk.AppProps) {
@@ -14,7 +16,18 @@ class CdkApp extends cdk.App {
       },
     };
 
+    new DnsStack(this, prefix("DnsStack"), stackProps);
     new AlarmStack(this, prefix("AlarmStack"), stackProps);
+  }
+}
+
+class DnsStack extends cdk.Stack {
+  constructor(scope: constructs.Construct, id: string, props: cdk.StackProps) {
+    super(scope, id, props);
+
+    new route53.HostedZone(this, "OtuvaHostedZone", {
+      zoneName: ssm.StringParameter.valueFromLookup(this, "/otuva/zoneName"),
+    });
   }
 }
 
