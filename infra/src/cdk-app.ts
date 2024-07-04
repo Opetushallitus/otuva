24bb23ecf131c5dd56f9ab27636de25a161ddd3c
Henry Heikkinen <henry.heikkinen@reaktor.com>
2024-07-04 13:50:45 +0300

    OPHYK-35 Add ALB in front of the Fargate service

--- infra/src/cdk-app.ts
+++ infra/src/cdk-app.ts
@@ -1,17 +1,21 @@
 import * as path from "node:path";
 
-import * as cdk from "aws-cdk-lib";
 import * as aws_sns from "aws-cdk-lib/aws-sns";
+import * as cdk from "aws-cdk-lib";
+import * as certificatemanager from "aws-cdk-lib/aws-certificatemanager";
 import * as constructs from "constructs";
-import * as route53 from "aws-cdk-lib/aws-route53";
-import * as ssm from "aws-cdk-lib/aws-ssm";
-import * as rds from "aws-cdk-lib/aws-rds";
 import * as ec2 from "aws-cdk-lib/aws-ec2";
 import * as ecr_assets from "aws-cdk-lib/aws-ecr-assets";
 import * as ecs from "aws-cdk-lib/aws-ecs";
+import * as elasticloadbalancingv2 from "aws-cdk-lib/aws-elasticloadbalancingv2";
 import * as logs from "aws-cdk-lib/aws-logs";
+import * as rds from "aws-cdk-lib/aws-rds";
+import * as route53 from "aws-cdk-lib/aws-route53";
+import * as route53_targets from "aws-cdk-lib/aws-route53-targets";
+import * as ssm from "aws-cdk-lib/aws-ssm";
 
 import { ALARM_TOPIC_ARN, prefix, QUALIFIER, VPC_NAME } from "./shared-account";
+import { getConfig, getEnvironment } from "./config";
 
 class CdkApp extends cdk.App {
   constructor(props: cdk.AppProps) {
@@ -36,10 +40,6 @@ class DnsStack extends cdk.Stack {
     new route53.HostedZone(this, "OtuvaHostedZone", {
       zoneName: ssm.StringParameter.valueFromLookup(this, "/otuva/zoneName"),
     });
-
-    route53.HostedZone.fromLookup(this, "YleiskayttoisetHostedZone", {
-      domainName: ssm.StringParameter.valueFromLookup(this, "zoneName"),
-    });
   }
 }
 
@@ -53,6 +53,7 @@ class AlarmStack extends cdk.Stack {
 class ApplicationStack extends cdk.Stack {
   constructor(scope: constructs.Construct, id: string, props: cdk.StackProps) {
     super(scope, id, props);
+    const config = getConfig();
     const vpc = ec2.Vpc.fromLookup(this, "Vpc", { vpcName: VPC_NAME });
 
     const dbSecurityGroup = new ec2.SecurityGroup(
@@ -110,12 +111,11 @@ class ApplicationStack extends cdk.Stack {
       },
     );
     const appPort = 8080;
-    const environmentName = process.env.ENV!;
     taskDefinition.addContainer("AppContainer", {
       image: ecs.ContainerImage.fromDockerImageAsset(dockerImage),
       logging: new ecs.AwsLogDriver({ logGroup, streamPrefix: "app" }),
       environment: {
-        ENV: environmentName === "hahtuva" ? "hahtuva2" : environmentName,
+        ENV: getEnvironment() === "hahtuva" ? "hahtuva2" : getEnvironment(),
         postgres_host: database.clusterEndpoint.hostname,
         postgres_port: database.clusterEndpoint.port.toString(),
         postgres_database: "kayttooikeus",
@@ -143,7 +143,14 @@ class ApplicationStack extends cdk.Stack {
           "CryptoserviceStaticSalt",
         ),
       },
-      portMappings: [{ containerPort: appPort, hostPort: appPort }],
+      portMappings: [
+        {
+          name: "kayttooikeus",
+          containerPort: appPort,
+          hostPort: appPort,
+          appProtocol: ecs.AppProtocol.http,
+        },
+      ],
     });
 
     const appSecurityGroup = new ec2.SecurityGroup(this, "AppSecurityGroup", {
@@ -153,17 +160,75 @@ class ApplicationStack extends cdk.Stack {
     const service = new ecs.FargateService(this, "Service", {
       cluster,
       taskDefinition,
-      desiredCount: 0,
+      desiredCount: config.desiredCount,
       maxHealthyPercent: 200,
       minHealthyPercent: 0,
       vpcSubnets: { subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS },
       securityGroups: [appSecurityGroup],
+      healthCheckGracePeriod: cdk.Duration.minutes(5),
     });
 
     dbSecurityGroup.addIngressRule(
       appSecurityGroup,
       ec2.Port.tcp(database.clusterEndpoint.port),
     );
+
+    const albSecurityGroup = new ec2.SecurityGroup(this, "AlbSecurityGroup", {
+      vpc,
+    });
+    const alb = new elasticloadbalancingv2.ApplicationLoadBalancer(
+      this,
+      "LoadBalancer",
+      {
+        vpc,
+        internetFacing: true,
+        securityGroup: albSecurityGroup,
+      },
+    );
+
+    const sharedHostedZone = route53.HostedZone.fromLookup(
+      this,
+      "YleiskayttoisetHostedZone",
+      {
+        domainName: ssm.StringParameter.valueFromLookup(this, "zoneName"),
+      },
+    );
+    const albHostname = `kayttooikeus.${sharedHostedZone.zoneName}`;
+    const albRecord = new route53.ARecord(this, "ALBARecord", {
+      zone: sharedHostedZone,
+      recordName: albHostname,
+      target: route53.RecordTarget.fromAlias(
+        new route53_targets.LoadBalancerTarget(alb),
+      ),
+    });
+
+    const albCertificate = new certificatemanager.Certificate(
+      this,
+      "AlbCertificate",
+      {
+        domainName: albHostname,
+        validation:
+          certificatemanager.CertificateValidation.fromDns(sharedHostedZone),
+      },
+    );
+
+    const listener = alb.addListener("Listener", {
+      protocol: elasticloadbalancingv2.ApplicationProtocol.HTTPS,
+      port: 443,
+      open: true,
+      certificates: [albCertificate],
+    });
+
+    listener.addTargets("ServiceTarget", {
+      port: appPort,
+      targets: [service],
+      healthCheck: {
+        enabled: true,
+        interval: cdk.Duration.seconds(10),
+        path: "/kayttooikeus-service/actuator/health",
+        port: appPort.toString(),
+      },
+    });
   }
 
   ssmSecret(name: string): ecs.Secret {
---
+++ infra/src/config.ts
@@ -0,0 +1,44 @@
+const environments = ["hahtuva", "dev", "qa", "prod"] as const;
+type EnvironmentName = (typeof environments)[number];
+
+const defaultConfig = {
+  desiredCount: 0,
+};
+
+export type Config = typeof defaultConfig;
+
+export function getEnvironment(): EnvironmentName {
+  const env = process.env.ENV;
+  if (!env) {
+    throw new Error("ENV environment variable is not set");
+  }
+  if (!contains(environments, env)) {
+    throw new Error(`Invalid environment name: ${env}`);
+  }
+  return env as EnvironmentName;
+}
+
+function contains(arr: readonly string[], value: string): boolean {
+  return arr.includes(value);
+}
+
+export function getConfig(): Config {
+  const env = getEnvironment();
+  return { hahtuva, dev, qa, prod }[env];
+}
+
+export const hahtuva: Config = {
+  ...defaultConfig,
+};
+
+export const dev: Config = {
+  ...defaultConfig,
+};
+
+export const qa: Config = {
+  ...defaultConfig,
+};
+
+export const prod: Config = {
+  ...defaultConfig,
+};
