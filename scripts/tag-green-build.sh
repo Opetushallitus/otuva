db1cb37dde44e73f44ceca96a1c72d5a069a069c
Henry Heikkinen <henry.heikkinen@reaktor.com>
2024-07-08 15:47:44 +0300

    OPHYK-35 Avoid redefining readonly repo variable in tag-green-build script

--- scripts/tag-green-build.sh
+++ scripts/tag-green-build.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 set -o errexit -o nounset -o pipefail
-readonly repo="$(cd "$(dirname "$0")/.." && pwd)"
-source "${repo}/scripts/lib/common-functions.sh"
+source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/lib/common-functions.sh"
 
 function force_push_tag {
   local -r tag="$1"
