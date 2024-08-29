c69a64ddc7d153946082c58ba665b28edaa2b315
Tomi Karlstedt <tomi.karlstedt@reaktor.com>
2024-08-29 12:17:20 +0300

    OPHYK-62 kotikuntahistoria kayttooikeus

---
+++ kayttooikeus-service/src/main/resources/db/migration/V20240829112233000__add_kotikuntahistoria_roles.sql
@@ -0,0 +1,2 @@
+SELECT insertkayttooikeus('OPPIJANUMEROREKISTERI', 'KOTIKUNTAHISTORIA', 'Lukuoikeus kotikuntahistoriaan');
+SELECT insertkayttooikeus('OPPIJANUMEROREKISTERI', 'TURVAKIELTOKOTIKUNTAHISTORIA', 'Lukuoikeus turvakiellollisten kotikuntahistoriaan');
