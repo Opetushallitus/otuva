d1e7bcff7e2e80c045a10c61fc8cab8969f689fa
Henry Heikkinen <henry.heikkinen@reaktor.com>
2024-09-19 15:11:48 +0300

    Remove now unused feature.accesslog-to-stdout flag

--- kayttooikeus-service/src/main/java/fi/vm/sade/kayttooikeus/config/AccessLogConfiguration.java
+++ kayttooikeus-service/src/main/java/fi/vm/sade/kayttooikeus/config/AccessLogConfiguration.java
@@ -1,9 +1,6 @@
 package fi.vm.sade.kayttooikeus.config;
 
 import ch.qos.logback.access.tomcat.LogbackValve;
-import org.apache.catalina.Context;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.web.embedded.tomcat.TomcatContextCustomizer;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.context.annotation.Bean;
@@ -11,29 +8,13 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AccessLogConfiguration {
-
     @Bean
-    @ConditionalOnProperty(name = "logback.access")
     public WebServerFactoryCustomizer containerCustomizer() {
         return container -> {
             if (container instanceof TomcatServletWebServerFactory) {
                 ((TomcatServletWebServerFactory) container).addContextCustomizers(context -> {
                     LogbackValve logbackValve = new LogbackValve();
                     logbackValve.setFilename("logback-access.xml");
-                    context.getPipeline().addValve(logbackValve);
-                });
-            }
-        };
-    }
-
-    @Bean
-    @ConditionalOnProperty(name = "feature.accesslog-to-stdout")
-    public WebServerFactoryCustomizer containerCustomizerAccessLogToStdout() {
-        return container -> {
-            if (container instanceof TomcatServletWebServerFactory) {
-                ((TomcatServletWebServerFactory) container).addContextCustomizers(context -> {
-                    LogbackValve logbackValve = new LogbackValve();
-                    logbackValve.setFilename("logback-access-stdout.xml");
                     context.getPipeline().addValve(logbackValve);
                 });
             }
