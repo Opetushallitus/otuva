e2a748e030a8b1492b504ce9d88f7d4e81739103
Tomi Karlstedt <tomi.karlstedt@reaktor.com>
2024-11-01 16:18:15 +0200

    OPHYK-204 replace properties with env variables before running tomcat

--- service-provider/start-local-env.sh
+++ service-provider/start-local-env.sh
@@ -7,7 +7,15 @@ function main {
   docker build -t service-provider .
   sp=$(cat $repo/metadata/hakasp.xml)
   keystore=$(base64 -i $repo/keystore/keystore.jks)
-  docker run -it -p 8080:8080 -e ENV=local -e hakasp="$sp" -e keystore="$keystore" --name service-provider-container service-provider
+  docker run -it -p 8080:8080 -e ENV=local -e hakasp="$sp" -e keystore="$keystore" \
+    -e ssm_keystore_password=m4JNeOMhcI42psiRyMS5 \
+    -e ssm_app_username_to_usermanagement=username \
+    -e ssm_app_password_to_usermanagement=password \
+    -e ssm_sp_keyalias=luokka_hakasp_selfsigned \
+    -e ssm_sp_keyalias_secondary=luokka_hakasp_selfsigned \
+    -e ssm_sp_keypassword=61rj9jtBPCwqe9cfbTwr \
+    -e ssm_mpassid_keyalias=local_mpassidtestsp_selfsigned_2023 \
+    --name service-provider-container service-provider
 }
 
 main "$@"
