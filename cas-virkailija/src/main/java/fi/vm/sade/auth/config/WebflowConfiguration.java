cedc8dee92930efb7a29282e4377dd9487114ba0
Tomi Karlstedt <tomi.karlstedt@reaktor.com>
2025-03-18 12:31:59 +0200

    OPHYK-530 add a webflow configurer that combines the old authToken login flow configuration and new configuration for delegated authentication

--- cas-virkailija/src/main/java/fi/vm/sade/auth/config/WebflowConfiguration.java
+++ cas-virkailija/src/main/java/fi/vm/sade/auth/config/WebflowConfiguration.java
@@ -1,4 +1,4 @@
-package fi.vm.sade.saml.action;
+package fi.vm.sade.auth.config;
 
 import org.apereo.cas.configuration.CasConfigurationProperties;
 import org.apereo.cas.web.flow.CasWebflowConstants;
@@ -9,24 +9,34 @@ import org.springframework.webflow.definition.registry.FlowDefinitionRegistry;
 import org.springframework.webflow.engine.ActionState;
 import org.springframework.webflow.engine.DecisionState;
 import org.springframework.webflow.engine.Flow;
+import org.springframework.webflow.engine.TransitionableState;
 import org.springframework.webflow.engine.builder.support.FlowBuilderServices;
 
-public class SAMLActionConfigurer extends AbstractCasWebflowConfigurer {
+import fi.vm.sade.saml.action.SAMLAction;
+import lombok.val;
 
+public class WebflowConfiguration extends AbstractCasWebflowConfigurer {
     private static final String STATE_ID_SAML_DECISION = "samlDecision";
     private static final String STATE_ID_SAML_ACTION = "samlAction";
     public static final String MFA_GAUTH_EVENT_ID = "mfa-gauth";
 
-    public SAMLActionConfigurer(FlowBuilderServices flowBuilderServices,
-                                FlowDefinitionRegistry loginFlowDefinitionRegistry,
-                                ConfigurableApplicationContext applicationContext,
-                                CasConfigurationProperties casProperties) {
+    public WebflowConfiguration(
+        final FlowBuilderServices flowBuilderServices,
+        final FlowDefinitionRegistry loginFlowDefinitionRegistry,
+        final ConfigurableApplicationContext applicationContext,
+        final CasConfigurationProperties casProperties) {
         super(flowBuilderServices, loginFlowDefinitionRegistry, applicationContext, casProperties);
+        setOrder(casProperties.getAuthn().getPac4j().getWebflow().getOrder() + 1);
     }
 
     @Override
     protected void doInitialize() {
-        Flow loginFlow = super.getLoginFlow();
+        val flowDefn = getLoginFlow();
+        configureAuthTokenLoginFlow(flowDefn);
+        configureDelegatedAuthenticationLoginFlow(flowDefn);
+    }
+
+    private void configureAuthTokenLoginFlow(Flow loginFlow) {
         StateDefinition originalStartState = loginFlow.getStartState();
 
         String decision = "requestParameters.authToken != null && !requestParameters.authToken.isEmpty()";
@@ -44,4 +54,13 @@ public class SAMLActionConfigurer extends AbstractCasWebflowConfigurer {
                 CasWebflowConstants.ACTION_ID_CLEAR_WEBFLOW_CREDENTIALS));
     }
 
+    private void configureDelegatedAuthenticationLoginFlow(Flow loginFlow) {
+        TransitionableState delegatedAuthenticationState = getState(loginFlow, CasWebflowConstants.STATE_ID_DELEGATED_AUTHENTICATION);
+
+        // Delegated authentication (Haka/Mpassid) should go through mfa
+        createTransitionForState(delegatedAuthenticationState, CasWebflowConstants.TRANSITION_ID_SUCCESS, MFA_GAUTH_EVENT_ID, true);
+
+        // Redirect back to login form on error
+        createTransitionForState(delegatedAuthenticationState, CasWebflowConstants.TRANSITION_ID_ERROR, CasWebflowConstants.STATE_ID_INIT_LOGIN_FORM, true);
+    }
 }
