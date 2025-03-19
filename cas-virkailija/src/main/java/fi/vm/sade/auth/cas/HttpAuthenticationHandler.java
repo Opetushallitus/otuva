bb3851287053dea215fb0dc7ca6a6e843b713c19
Tomi Karlstedt <tomi.karlstedt@reaktor.com>
2025-03-19 16:18:41 +0200

    OPHYK-530 remove unused stuff and use records

--- cas-virkailija/src/main/java/fi/vm/sade/auth/cas/HttpAuthenticationHandler.java
+++ cas-virkailija/src/main/java/fi/vm/sade/auth/cas/HttpAuthenticationHandler.java
@@ -60,34 +60,7 @@ public class HttpAuthenticationHandler extends AbstractUsernamePasswordAuthentic
                 });
     }
 
-    private static class LoginDto {
-        private String username;
-        private String password;
-
-        public LoginDto() {
-        }
-
-        public LoginDto(String username, String password) {
-            this.username = username;
-            this.password = password;
-        }
-
-        public String getUsername() {
-            return username;
-        }
-
-        public void setUsername(String username) {
-            this.username = username;
-        }
-
-        public String getPassword() {
-            return password;
-        }
-
-        public void setPassword(String password) {
-            this.password = password;
-        }
-    }
+    private record LoginDto(String username, String password) {}
 
     @Data
     private static class KayttajatiedotReadDto {
