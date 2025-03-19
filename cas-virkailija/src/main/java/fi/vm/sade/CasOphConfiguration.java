e9c5df5d433ba1c4acb246d0bafcbaa36697e0e0
Tomi Karlstedt <tomi.karlstedt@reaktor.com>
2025-03-19 15:13:15 +0200

    OPHYK-530 replace custom authentication handler with a principal preprocessor for delegated authentication

--- cas-virkailija/src/main/java/fi/vm/sade/CasOphConfiguration.java
+++ cas-virkailija/src/main/java/fi/vm/sade/CasOphConfiguration.java
@@ -1,16 +1,32 @@
 package fi.vm.sade;
 
 import io.micrometer.observation.ObservationRegistry;
+import lombok.RequiredArgsConstructor;
+
+import org.apereo.cas.authentication.principal.DelegatedAuthenticationPreProcessor;
+import org.apereo.cas.authentication.principal.PrincipalFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+import fi.vm.sade.auth.cas.DelegatedAuthenticationProcessor;
+import fi.vm.sade.auth.clients.KayttooikeusRestClient;
+
 @Configuration
 @ComponentScan
+@RequiredArgsConstructor
 public class CasOphConfiguration {
+    final PrincipalFactory principalFactory;
+    final KayttooikeusRestClient kayttooikeusRestClient;
+
     @Bean
     public ObservationRegistry observationRegistry() {
         // Disable all observations
         return ObservationRegistry.NOOP;
+    }
+
+    @Bean
+    public DelegatedAuthenticationPreProcessor myProcessor() {
+        return new DelegatedAuthenticationProcessor(principalFactory, kayttooikeusRestClient);
     }
 }
--- cas-virkailija/src/main/java/fi/vm/sade/auth/config/AuthenticationEventExecutionPlanConfiguration.java
+++ cas-virkailija/src/main/java/fi/vm/sade/auth/config/AuthenticationEventExecutionPlanConfiguration.java
@@ -1,22 +1,13 @@
 package fi.vm.sade.auth.config;
 
-import fi.vm.sade.auth.cas.DelegatedAuthenticationHandler;
 import fi.vm.sade.auth.cas.HttpAuthenticationHandler;
 import fi.vm.sade.auth.clients.KayttooikeusRestClient;
 import fi.vm.sade.javautils.httpclient.OphHttpClient;
 import fi.vm.sade.saml.action.SAMLAuthenticationHandler;
-import lombok.val;
 
 import org.apereo.cas.authentication.AuthenticationEventExecutionPlan;
 import org.apereo.cas.authentication.AuthenticationEventExecutionPlanConfigurer;
-import org.apereo.cas.authentication.principal.PrincipalFactory;
-import org.apereo.cas.authentication.principal.provision.DelegatedClientUserProfileProvisioner;
-import org.apereo.cas.configuration.CasConfigurationProperties;
-import org.apereo.cas.pac4j.client.DelegatedIdentityProviders;
 import org.apereo.cas.services.ServicesManager;
-import org.pac4j.core.context.session.SessionStore;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -24,43 +15,20 @@ public class AuthenticationEventExecutionPlanConfiguration implements Authentica
     private final ServicesManager servicesManager;
     private final OphHttpClient httpClient;
     private final KayttooikeusRestClient kayttooikeusRestClient;
-    final ConfigurableApplicationContext applicationContext;
-    final CasConfigurationProperties casProperties;
-    final PrincipalFactory clientPrincipalFactory;
-    final DelegatedIdentityProviders identityProviders;
-    final DelegatedClientUserProfileProvisioner clientUserProfileProvisioner;
-    final SessionStore delegatedClientDistributedSessionStore;
 
     public AuthenticationEventExecutionPlanConfiguration(
             ServicesManager servicesManager,
             OphHttpClient httpClient,
-            KayttooikeusRestClient kayttooikeusRestClient,
-            final ConfigurableApplicationContext applicationContext,
-            final CasConfigurationProperties casProperties,
-            @Qualifier("clientPrincipalFactory") final PrincipalFactory clientPrincipalFactory,
-            @Qualifier("delegatedIdentityProviders") final DelegatedIdentityProviders identityProviders,
-            @Qualifier("clientUserProfileProvisioner") final DelegatedClientUserProfileProvisioner clientUserProfileProvisioner,
-            @Qualifier("delegatedClientDistributedSessionStore") final SessionStore delegatedClientDistributedSessionStore) {
+            KayttooikeusRestClient kayttooikeusRestClient) {
         this.servicesManager = servicesManager;
         this.httpClient = httpClient;
         this.kayttooikeusRestClient = kayttooikeusRestClient;
-        this.applicationContext = applicationContext;
-        this.casProperties = casProperties;
-        this.clientPrincipalFactory = clientPrincipalFactory;
-        this.identityProviders = identityProviders;
-        this.clientUserProfileProvisioner = clientUserProfileProvisioner;
-        this.delegatedClientDistributedSessionStore = delegatedClientDistributedSessionStore;
     }
 
     @Override
     public void configureAuthenticationExecutionPlan(AuthenticationEventExecutionPlan plan) {
         plan.registerAuthenticationHandler(new HttpAuthenticationHandler(servicesManager, 1, httpClient));
         plan.registerAuthenticationHandler(new SAMLAuthenticationHandler(2, kayttooikeusRestClient));
-
-        val pac4j = casProperties.getAuthn().getPac4j().getCore();
-        plan.registerAuthenticationHandler(new DelegatedAuthenticationHandler(pac4j,
-                servicesManager, clientPrincipalFactory, identityProviders, clientUserProfileProvisioner,
-                delegatedClientDistributedSessionStore, applicationContext, kayttooikeusRestClient));
     }
 
 }
