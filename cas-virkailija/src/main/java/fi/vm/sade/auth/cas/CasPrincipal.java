73ff1876f7c178d5a50d0f97449aea631965700a
Tomi Karlstedt <tomi.karlstedt@reaktor.com>
2025-04-04 13:52:51 +0300

    OPHYK-578 fix setting the temporary Haka registration token for the interrupt inquirer

--- cas-virkailija/src/main/java/fi/vm/sade/auth/cas/CasPrincipal.java
+++ cas-virkailija/src/main/java/fi/vm/sade/auth/cas/CasPrincipal.java
@@ -3,18 +3,23 @@ package fi.vm.sade.auth.cas;
 import org.apereo.cas.authentication.principal.Principal;
 import org.apereo.cas.authentication.principal.PrincipalFactory;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 public class CasPrincipal {
     public static Principal of(PrincipalFactory principalFactory, CasUserAttributes userAttributes) {
         try {
+            List<Object> hakaRegistrationToken = new ArrayList<>();
+            if (userAttributes.hakaRegistrationToken() != null && userAttributes.hakaRegistrationToken().isPresent()) {
+                hakaRegistrationToken.add(userAttributes.hakaRegistrationToken().get());
+            }
             Map<String, List<Object>> attributes = Map.of(
                     "oidHenkilo", List.of(userAttributes.oidHenkilo()),
                     "idpEntityId", List.of(userAttributes.idpEntityId().orElse("usernamePassword")),
                     "kayttajaTyyppi", List.of(userAttributes.kayttajaTyyppi().orElse("VIRKAILIJA")),
                     "roles", List.copyOf(userAttributes.roles()),
-                    "hakaRegistrationToken", List.of(userAttributes.hakaRegistrationToken())
+                    "hakaRegistrationToken", hakaRegistrationToken
             );
             return principalFactory.createPrincipal(userAttributes.username(), attributes);
         } catch (Throwable t) {
