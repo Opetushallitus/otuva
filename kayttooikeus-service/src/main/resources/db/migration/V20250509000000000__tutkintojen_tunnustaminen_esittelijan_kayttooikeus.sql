a7051aedd47069a178f126dd45cae6fd3643415a
Henry Heikkinen <henry.heikkinen@reaktor.com>
2025-05-09 15:04:43 +0300

    OPHYK-635 Add käyttöoikeus for new Tutkintojen tunnustaminen service

---
+++ kayttooikeus-service/src/main/resources/db/migration/V20250509000000000__tutkintojen_tunnustaminen_esittelijan_kayttooikeus.sql
@@ -0,0 +1,2 @@
+SELECT insertpalvelu('TUTU', 'Tutkintojen tunnustamisen virkailijakäyttöliittymä');
+SELECT insertkayttooikeus('TUTU', 'ESITTELIJA', 'Saa käyttää Tutkintojen tunnustamisen käyttöliittymää');
