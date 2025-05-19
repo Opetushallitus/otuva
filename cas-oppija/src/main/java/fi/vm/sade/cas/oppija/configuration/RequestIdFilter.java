1a0aa096215b0bb9537120092515ddf7c9bbe7eb
Henry Heikkinen <henry.heikkinen@reaktor.com>
2025-05-19 12:58:13 +0300

    OPHYK-562 Add RequestIdFilter to log request IDs for tracing
    Implement RequestIdFilter to extract or generate request IDs, ensuring they are logged for better traceability. Update access log pattern to include requestId.

---
+++ cas-oppija/src/main/java/fi/vm/sade/cas/oppija/configuration/RequestIdFilter.java
@@ -0,0 +1,44 @@
+package fi.vm.sade.cas.oppija.configuration;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.Optional;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+@Slf4j
+public class RequestIdFilter implements Filter {
+    public static final String REQUEST_ID_ATTRIBUTE = RequestIdFilter.class.getName() + ".requestId";
+    public static final String REQUEST_ID_HEADER = "x-amzn-trace-id";
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        try {
+            var requestId = getFromTraceHeader(request).orElseGet(RequestIdFilter::generateRequestId);
+            MDC.put(REQUEST_ID_ATTRIBUTE, requestId);
+            request.setAttribute(REQUEST_ID_ATTRIBUTE, requestId);
+            chain.doFilter(request, response);
+        } finally {
+            MDC.remove(REQUEST_ID_ATTRIBUTE);
+        }
+    }
+
+    public static Optional<String> getFromTraceHeader(ServletRequest request) {
+        if (request instanceof HttpServletRequest) {
+            var httpRequest = (HttpServletRequest) request;
+            return Optional.ofNullable(httpRequest.getHeader(REQUEST_ID_HEADER));
+        }
+        return Optional.empty();
+    }
+
+    public static String generateRequestId() {
+        return java.util.UUID.randomUUID().toString();
+    }
+}
