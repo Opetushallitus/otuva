e1d9ed10fa86159b27d619544c22e29eb3e57e33
Tomi Karlstedt <tomi.karlstedt@reaktor.com>
2025-05-20 14:46:28 +0300

    deploy hahtuva from cas-7.2 branch

--- infra/src/cdk-app-util.ts
+++ infra/src/cdk-app-util.ts
@@ -45,7 +45,7 @@ class ContinousDeploymentStack extends cdk.Stack {
       legacyPrefix("HahtuvaContinuousDeploymentPipeline"),
       connection,
       "hahtuva",
-      { owner: "Opetushallitus", name: "otuva", branch: "master" },
+      { owner: "Opetushallitus", name: "otuva", branch: "cas-7.2" },
       props
     );
     new ContinousDeploymentPipelineStack(
@@ -53,7 +53,7 @@ class ContinousDeploymentStack extends cdk.Stack {
       legacyPrefix("DevContinuousDeploymentPipeline"),
       connection,
       "dev",
-      { owner: "Opetushallitus", name: "otuva", branch: "green-hahtuva" },
+      { owner: "Opetushallitus", name: "otuva", branch: "master" },
       props
     );
     new ContinousDeploymentPipelineStack(
@@ -119,7 +119,7 @@ class ContinousDeploymentPipelineStack extends cdk.Stack {
     const sourceStage = pipeline.addStage({ stageName: "Source" });
     sourceStage.addAction(sourceAction);
 
-    const runTests = env === "hahtuva";
+    const runTests = env === "hahtuva" || env === "dev";
     if (runTests) {
       const testStage = pipeline.addStage({ stageName: "Test" });
       testStage.addAction(
