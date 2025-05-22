a06689191aa4d2a3e352abd0d8b7bd827a049fc0
Tomi Karlstedt <tomi.karlstedt@reaktor.com>
2025-05-22 11:42:26 +0300

    OPHYK-585 override DelegatedAuthenticationClientLogoutRequest so that Jackson is able to deserialize it from ticket registry

---
+++ cas-virkailija/src/main/java/org/apereo/cas/support/pac4j/authentication/DelegatedAuthenticationClientLogoutRequest.java
@@ -0,0 +1,36 @@
+package org.apereo.cas.support.pac4j.authentication;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+import com.fasterxml.jackson.annotation.JsonFilter;
+
+/**
+ * Overrides DelegatedAuthenticationClientLogoutRequest from CAS since this class is serialized and
+ * deserialized to transient tickets during SAML authentication and it requires additional constructors
+ * to be compliant with Jackson deserialization.
+ */
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@JsonFilter("DelegatedAuthenticationClientLogoutRequest")
+public class DelegatedAuthenticationClientLogoutRequest implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 4280830773566610694L;
+
+    private int status;
+
+    private String message;
+
+    private String location;
+
+    private String target;
+}
