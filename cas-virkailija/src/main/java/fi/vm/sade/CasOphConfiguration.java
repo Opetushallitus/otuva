a5210207255f2037f77e864479dd44ad4936d260
Tomi Karlstedt <tomi.karlstedt@reaktor.com>
2025-09-15 13:49:04 +0300

    OPHYK-177 add a custom DelegatedSaml2ClientLogoutAction to fix missing transaction with JPA ticket registry

--- cas-virkailija/src/main/java/fi/vm/sade/CasOphConfiguration.java
+++ cas-virkailija/src/main/java/fi/vm/sade/CasOphConfiguration.java
@@ -5,14 +5,23 @@ import lombok.RequiredArgsConstructor;
 
 import org.apereo.cas.authentication.principal.DelegatedAuthenticationPreProcessor;
 import org.apereo.cas.authentication.principal.PrincipalFactory;
+import org.apereo.cas.configuration.CasConfigurationProperties;
+import org.apereo.cas.logout.slo.SingleLogoutRequestExecutor;
 import org.apereo.cas.pac4j.client.DelegatedClientIdentityProviderRedirectionStrategy;
+import org.apereo.cas.ticket.registry.TicketRegistry;
 import org.apereo.cas.ticket.serialization.TicketSerializationExecutionPlanConfigurer;
+import org.apereo.cas.web.flow.CasWebflowConstants;
+import org.apereo.cas.web.flow.actions.WebflowActionBeanSupplier;
+import org.apereo.cas.web.flow.actions.logout.DelegatedSaml2ClientLogoutAction;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.core.env.Environment;
+import org.springframework.webflow.execution.Action;
 
 import fi.vm.sade.auth.cas.DelegatedIdpRedirectionStrategy;
 import fi.vm.sade.auth.cas.OtuvaDelegatedAuthenticationProcessor;
@@ -71,5 +80,23 @@ public class CasOphConfiguration {
     @Bean
     TomcatGate tomcatGate() {
         return new TomcatGate();
+    }
+
+    @Bean(name = CasWebflowConstants.ACTION_ID_DELEGATED_AUTHENTICATION_SAML2_CLIENT_LOGOUT)
+    @RefreshScope(proxyMode = ScopedProxyMode.DEFAULT)
+    public Action delegatedSaml2ClientLogoutAction(
+        final CasConfigurationProperties casProperties,
+        final ConfigurableApplicationContext applicationContext,
+        @Qualifier(TicketRegistry.BEAN_NAME)
+        final TicketRegistry ticketRegistry,
+        @Qualifier(SingleLogoutRequestExecutor.BEAN_NAME)
+        final SingleLogoutRequestExecutor singleLogoutRequestExecutor) {
+        return WebflowActionBeanSupplier.builder()
+            .withApplicationContext(applicationContext)
+            .withProperties(casProperties)
+            .withAction(() -> new DelegatedSaml2ClientLogoutAction(ticketRegistry, singleLogoutRequestExecutor))
+            .withId(CasWebflowConstants.ACTION_ID_DELEGATED_AUTHENTICATION_SAML2_CLIENT_LOGOUT)
+            .build()
+            .get();
     }
 }
