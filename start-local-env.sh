ceb98904124a3bf88b8545796694e1d8aaa698ec
Henry Heikkinen <henry.heikkinen@reaktor.com>
2025-10-09 10:12:50 +0300

    Add top-level start-local-env.sh

---
+++ start-local-env.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+set -o errexit -o nounset -o pipefail -o xtrace
+readonly repo="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+function main {
+  cd "$repo"
+  local -r session="$( basename "$( pwd )" )"
+  tmux kill-session -t "$session" || true
+  tmux start-server
+  tmux new-session -d -s "$session"
+  
+  tmux select-pane -t 0
+  tmux splitw -h
+  tmux select-pane -t 0
+  tmux splitw -v
+  tmux splitw -v
+  tmux select-pane -t 3
+  tmux splitw -v
+  tmux splitw -v
+  # +---+---+
+  # | 0 | 3 |
+  # +---+ - +
+  # | 1 | 4 |
+  # +---+ - +
+  # | 2 | 5 |
+  # +---+---+
+
+  tmux select-pane -t 0
+  tmux send-keys "$repo/cas-oppija/scripts/run-database.sh" C-m
+
+  tmux select-pane -t 1
+  tmux send-keys "while sleep 0.5; do $repo/cas-oppija/scripts/run-cas-java-11.sh; done" C-m
+
+  tmux select-pane -t 2
+  tmux send-keys "$repo/cas-oppija/scripts/open-browser.sh; exit" C-m
+
+  tmux select-pane -t 3
+  tmux send-keys "$repo/kayttooikeus-service/scripts/run-database.sh" C-m
+
+  tmux select-pane -t 4
+  tmux send-keys "$repo/kayttooikeus-service/scripts/run-kayttooikeus.sh" C-m
+
+  tmux attach-session -t "$session"
+}
+
+main "$@"
