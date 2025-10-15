6428ffb664da13f15ba2836a08b5766a8387ffdc
Timo Suomela <timo.suomela@reaktor.com>
2025-10-15 14:38:02 +0300

    OPHYK-802 Adjust code to changes in cas 7.3.0 internals

--- cas-virkailija/src/main/java/fi/vm/sade/auth/config/AuthenticationEventExecutionPlanConfiguration.java
+++ cas-virkailija/src/main/java/fi/vm/sade/auth/config/AuthenticationEventExecutionPlanConfiguration.java
@@ -7,27 +7,23 @@ import fi.vm.sade.saml.action.SAMLAuthenticationHandler;
 
 import org.apereo.cas.authentication.AuthenticationEventExecutionPlan;
 import org.apereo.cas.authentication.AuthenticationEventExecutionPlanConfigurer;
-import org.apereo.cas.services.ServicesManager;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AuthenticationEventExecutionPlanConfiguration implements AuthenticationEventExecutionPlanConfigurer {
-    private final ServicesManager servicesManager;
     private final OphHttpClient httpClient;
     private final KayttooikeusRestClient kayttooikeusRestClient;
 
     public AuthenticationEventExecutionPlanConfiguration(
-            ServicesManager servicesManager,
             OphHttpClient httpClient,
             KayttooikeusRestClient kayttooikeusRestClient) {
-        this.servicesManager = servicesManager;
         this.httpClient = httpClient;
         this.kayttooikeusRestClient = kayttooikeusRestClient;
     }
 
     @Override
     public void configureAuthenticationExecutionPlan(AuthenticationEventExecutionPlan plan) {
-        plan.registerAuthenticationHandler(new HttpAuthenticationHandler(servicesManager, 1, httpClient));
+        plan.registerAuthenticationHandler(new HttpAuthenticationHandler(1, httpClient));
         plan.registerAuthenticationHandler(new SAMLAuthenticationHandler(2, kayttooikeusRestClient));
     }
 
