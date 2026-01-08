fe9a0c4099ec5317de09b0175541087e88fac358
Tomi Karlstedt <tomi.karlstedt@reaktor.com>
2026-01-08 12:01:24 +0200

    OPHYK-766 remove GET /userDetails test

--- kayttooikeus-service/src/test/java/fi/vm/sade/kayttooikeus/controller/UserDetailsControllerTest.java
+++ kayttooikeus-service/src/test/java/fi/vm/sade/kayttooikeus/controller/UserDetailsControllerTest.java
@@ -6,32 +6,21 @@ import fi.vm.sade.kayttooikeus.service.exception.UnauthorizedException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.http.MediaType;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
 public class UserDetailsControllerTest extends AbstractControllerTest {
-
-    @MockitoBean
-    private UserDetailsService userDetailsService;
     @MockitoBean
     private KayttajarooliProvider kayttajaRooliProvider;
     @MockitoBean
     private KayttajatiedotService kayttajatiedotService;
-
-    @Test
-    public void getUserDetailsReturnsOkWithoutAuthentication() throws Exception {
-        mvc.perform(get("/userDetails/user1"))
-                .andExpect(status().isGone());
-    }
 
     @Test
     public void getByUsernameAndPasswordReturnsUnauthorizedWithWrongUserPass() throws Exception {
