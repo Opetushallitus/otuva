45b9b3cceb860aa565627b67541b5fdcbd47c2c9
Tomi Karlstedt <tomi.karlstedt@reaktor.com>
2026-01-08 11:52:35 +0200

    OPHYK-766 remove /userDetails endpoint

--- kayttooikeus-service/src/main/java/fi/vm/sade/kayttooikeus/controller/UserDetailsController.java
+++ kayttooikeus-service/src/main/java/fi/vm/sade/kayttooikeus/controller/UserDetailsController.java
@@ -4,16 +4,10 @@ import fi.vm.sade.kayttooikeus.CasUserAttributes;
 import fi.vm.sade.kayttooikeus.dto.LoginDto;
 import fi.vm.sade.kayttooikeus.model.Kayttajatiedot;
 import fi.vm.sade.kayttooikeus.service.KayttajatiedotService;
-import fi.vm.sade.kayttooikeus.service.exception.GoneException;
-import fi.vm.sade.kayttooikeus.service.exception.NotFoundException;
 import io.swagger.v3.oas.annotations.Hidden;
 import lombok.RequiredArgsConstructor;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.*;
 
 import jakarta.validation.Valid;
@@ -23,25 +17,7 @@ import jakarta.validation.Valid;
 @RequestMapping(value = "/userDetails", produces = MediaType.APPLICATION_JSON_VALUE)
 @RequiredArgsConstructor
 public class UserDetailsController {
-
-    private final UserDetailsService userDetailsService;
     private final KayttajatiedotService kayttajatiedotService;
-
-    @Value("${kayttooikeus.userdetails.enabled}")
-    public Boolean userdetailsEnabled;
-
-    // Palomuurilla rajoitettu pääsy vain verkon sisältä
-    @GetMapping("/{username}")
-    public UserDetails getUserDetails(@PathVariable String username) {
-        if (!userdetailsEnabled) {
-            throw new GoneException("endpoint disabled (see: https://wiki.eduuni.fi/x/kc8hHw)");
-        }
-        try {
-            return userDetailsService.loadUserByUsername(username);
-        } catch (UsernameNotFoundException e) {
-            throw new NotFoundException(e);
-        }
-    }
 
     // Palomuurilla rajoitettu pääsy vain verkon sisältä
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
--- kayttooikeus-service/src/main/java/fi/vm/sade/kayttooikeus/service/KayttajarooliProvider.java
+++ kayttooikeus-service/src/main/java/fi/vm/sade/kayttooikeus/service/KayttajarooliProvider.java
@@ -1,12 +1,70 @@
 package fi.vm.sade.kayttooikeus.service;
 
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Stream;
 
-public interface KayttajarooliProvider {
+import org.springframework.stereotype.Component;
 
-    Set<String> getByKayttajaOid(String kayttajaOid);
+import fi.vm.sade.kayttooikeus.dto.OrganisaatioPalveluRooliDto;
+import fi.vm.sade.kayttooikeus.repositories.MyonnettyKayttoOikeusRyhmaTapahtumaRepository;
+import lombok.RequiredArgsConstructor;
 
-    Map<String, Set<String>> getRolesByOrganisation(String kayttajaOid);
+import static java.util.stream.Collectors.toSet;
 
+import java.util.HashMap;
+
+@Component
+@RequiredArgsConstructor
+public class KayttajarooliProvider {
+    private final MyonnettyKayttoOikeusRyhmaTapahtumaRepository myonnettyKayttoOikeusRyhmaTapahtumaRepository;
+
+    public Set<String> getByKayttajaOid(String kayttajaOid) {
+        return streamRooliByKayttajaOid(kayttajaOid).collect(toSet());
+    }
+
+    Stream<String> streamRooliByKayttajaOid(String kayttajaOid) {
+        return myonnettyKayttoOikeusRyhmaTapahtumaRepository.findOrganisaatioPalveluRooliByOid(kayttajaOid)
+                .stream()
+                .flatMap(this::getRoolit)
+                .map(String::toUpperCase);
+    }
+
+    public Map<String, Set<String>> getRolesByOrganisation(String kayttajaOid) {
+        var roles = new HashMap<String, Set<String>>();
+        myonnettyKayttoOikeusRyhmaTapahtumaRepository.findOrganisaatioPalveluRooliByOid(kayttajaOid)
+            .stream()
+            .forEach(dto -> {
+                var role = dto.getPalvelu() + "_" + dto.getRooli();
+                var cur = roles.get(dto.getOrganisaatioOid());
+                if (cur == null) {
+                    cur = new HashSet<String>();
+                }
+                cur.add(role);
+                roles.put(dto.getOrganisaatioOid(), cur);
+            });
+        return roles;
+    }
+
+    private Stream<String> getRoolit(OrganisaatioPalveluRooliDto dto) {
+        Set<String> roolit = new HashSet<>();
+
+        StringBuilder builder = new StringBuilder("ROLE_APP_");
+        // ROLE_APP_<palvelu>
+        builder.append(dto.getPalvelu());
+        roolit.add(builder.toString());
+
+        // ROLE_APP_<palvelu>_<kayttooikeus_rooli>
+        builder.append("_");
+        builder.append(dto.getRooli());
+        roolit.add(builder.toString());
+
+        // ROLE_APP_<palvelu>_<kayttooikeus_rooli>_<organisaatiooid>
+        builder.append("_");
+        builder.append(dto.getOrganisaatioOid());
+        roolit.add(builder.toString());
+
+        return roolit.stream();
+    }
 }
