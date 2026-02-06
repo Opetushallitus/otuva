17740f3738bc521d9148a098e452cd8b268e3b2c
Timo Suomela <timo.suomela@reaktor.com>
2026-02-06 15:21:53 +0200

    OPHYK-985 Add kayttooikeus for tiedotuspalvelu

---
+++ kayttooikeus-service/src/main/resources/db/migration/V20260206151518000__tiedotuspalvelu_kielitutkinto.sql
@@ -0,0 +1,2 @@
+SELECT insertpalvelu('TIEDOTUSPALVELU', 'Tiedotuspalvelu');
+SELECT insertkayttooikeus('TIEDOTUSPALVELU', 'KIELITUTKINTOTODISTUS_TIEDOTE', 'Oikeus luoda tiedote joka koske kielitutkinnon todistusta');
