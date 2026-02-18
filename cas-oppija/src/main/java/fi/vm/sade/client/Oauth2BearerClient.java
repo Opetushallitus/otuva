69f8ba9206b92ec8691279e35498e5ed7b3054e5
Tomi Karlstedt <tomi.karlstedt@reaktor.com>
2026-02-18 16:09:12 +0200

    OPHYK-959 upgrade cas-oppija to 7.3, remove valtuudet feature, and switch to Oauth2 protocol for fetching oid

---
+++ cas-oppija/src/main/java/fi/vm/sade/client/Oauth2BearerClient.java
@@ -0,0 +1,66 @@
+package fi.vm.sade.client;
+
+import static fi.vm.sade.CacheConfiguration.CACHE_NAME_OAUTH2_BEARER;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Component;
+
+import com.google.gson.Gson;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+@CacheConfig(cacheNames = CACHE_NAME_OAUTH2_BEARER)
+@RequiredArgsConstructor
+public class Oauth2BearerClient implements HttpFormEncoding {
+    private final Gson gson = new Gson();
+
+    @Value("${kayttooikeus.baseurl}")
+    private String kayttooikeusBaseurl;
+    @Value("${oauth2.client-id}")
+    private String clientId;
+    @Value("${oauth2.client-secret}")
+    private String clientSecret;
+
+    @Cacheable(value = CACHE_NAME_OAUTH2_BEARER, sync = true)
+    public String getOauth2Bearer() throws IOException, InterruptedException {
+        String tokenUrl = kayttooikeusBaseurl + "/oauth2/token";
+        LOGGER.info("refetching oauth2 bearer from " + tokenUrl);
+        var request = HttpRequest.newBuilder()
+                .uri(URI.create(tokenUrl))
+                .header("Content-Type", "application/x-www-form-urlencoded")
+                .POST(encodeFormBody(Map.of(
+                        "grant_type", "client_credentials",
+                        "client_id", clientId,
+                        "client_secret", clientSecret
+                )))
+                .build();
+        var client = HttpClient.newHttpClient();
+        HttpResponse<String> res = client.send(request, BodyHandlers.ofString());
+        if (res.statusCode() != 200) {
+            throw new RuntimeException("Oauth2 bearer returned status code " + res.statusCode() + ": " + res.body());
+        }
+        LOGGER.info("oauth2 bearer body: " + res.body());
+        return gson.fromJson(res.body(), Oauth2Token.class).access_token();
+    }
+
+    @CacheEvict(value = CACHE_NAME_OAUTH2_BEARER, allEntries = true)
+    public void evictOauth2Bearer() {
+        LOGGER.info("evicting oauth2 bearer cache");
+    }
+
+    record Oauth2Token(String access_token, String token_type, Integer expires_in) {}
+}
