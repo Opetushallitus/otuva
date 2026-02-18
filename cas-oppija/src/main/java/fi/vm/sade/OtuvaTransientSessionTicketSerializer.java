1a70e0888882e1f27b813a0df149d5e2a0334b8f
Tomi Karlstedt <tomi.karlstedt@reaktor.com>
2026-02-18 16:09:12 +0200

    OPHYK-959 upgrade cas-oppija to 7.3, remove valtuudet feature, and switch to Oauth2 protocol for fetching oid

---
+++ cas-oppija/src/main/java/fi/vm/sade/OtuvaTransientSessionTicketSerializer.java
@@ -0,0 +1,149 @@
+package fi.vm.sade;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.Reader;
+import java.io.Writer;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Base64;
+import java.util.Collection;
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+import org.apereo.cas.ticket.TransientSessionTicketImpl;
+import org.apereo.cas.util.serialization.StringSerializer;
+import org.apereo.cas.util.serialization.SerializationUtils;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class OtuvaTransientSessionTicketSerializer implements StringSerializer<TransientSessionTicketImpl> {
+    private static final Charset charset = StandardCharsets.UTF_8;
+    private final Base64.Encoder encoder = Base64.getEncoder();
+    private final Base64.Decoder decoder = Base64.getDecoder();
+
+    public OtuvaTransientSessionTicketSerializer() {
+        LOGGER.trace("Initializing OtuvaTransientSessionTicketSerializer");
+    }
+
+    @Override
+    public TransientSessionTicketImpl from(String s) {
+        LOGGER.trace("from String");
+        return fromBase64String(s);
+    }
+
+    @Override
+    public TransientSessionTicketImpl from(Reader reader) {
+        LOGGER.trace("from Reader");
+        try {
+            return fromBase64String(IOUtils.toString(reader));
+        } catch (IOException e) {
+            return null;
+        }
+    }
+
+    @Override
+    public TransientSessionTicketImpl from(InputStream stream) {
+        LOGGER.trace("from InputStream");
+        try {
+            return fromBase64String(IOUtils.toString(stream, charset));
+        } catch (IOException e) {
+            return null;
+        }
+    }
+
+    @Override
+    public TransientSessionTicketImpl from(File file) {
+        LOGGER.trace("from File");
+        try (FileInputStream fis = new FileInputStream(file)) {
+            return from(fis);
+        } catch (IOException e) {
+            return null;
+        }
+    }
+
+    @Override
+    public TransientSessionTicketImpl from(Writer writer) {
+        LOGGER.trace("from Writer");
+        // Seems like a bug as toString is inherited from java.lang.Object but AbstractJacksonBackedSerializer does the same
+        return from(writer.toString());
+    }
+
+    private TransientSessionTicketImpl fromBase64String(String s) {
+        byte[] bytes = decoder.decode(s.getBytes(StandardCharsets.UTF_8));
+        return SerializationUtils.deserialize(bytes, TransientSessionTicketImpl.class);
+    }
+
+    @Override
+    public void to(OutputStream out, TransientSessionTicketImpl object) {
+        LOGGER.trace("to OutputStream");
+        try {
+            IOUtils.write(toBase64String(object), out, charset);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void to(Writer out, TransientSessionTicketImpl object) {
+        LOGGER.trace("to Writer");
+        try {
+            IOUtils.write(toBase64String(object), out);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void to(File out, TransientSessionTicketImpl object) {
+        LOGGER.trace("to File");
+        try {
+            FileUtils.write(out, toBase64String(object), charset);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private String toBase64String(TransientSessionTicketImpl object) {
+        return encoder.encodeToString(SerializationUtils.serialize(object));
+    }
+
+    @Override
+    public String toString(TransientSessionTicketImpl object) {
+        LOGGER.trace("to String");
+        return toBase64String(object);
+    }
+
+    @Override
+    public Class<TransientSessionTicketImpl> getTypeToSerialize() {
+        return TransientSessionTicketImpl.class;
+    }
+
+    @Override
+    public List<TransientSessionTicketImpl> fromList(String s) {
+        LOGGER.trace("List from String");
+        byte[] bytes = decoder.decode(s.getBytes(StandardCharsets.UTF_8));
+        return SerializationUtils.deserialize(bytes, List.class);
+    }
+
+    @Override
+    public String fromList(Collection<TransientSessionTicketImpl> json) {
+        LOGGER.trace("List to String");
+        var list = new ArrayList<>();
+        list.addAll(json);
+        return encoder.encodeToString(SerializationUtils.serialize(list));
+    }
+
+    @Override
+    public TransientSessionTicketImpl merge(TransientSessionTicketImpl baseEntity,
+            TransientSessionTicketImpl childEntity) {
+        LOGGER.trace("merge " + baseEntity.getId() + " to " + childEntity.getId());
+        return childEntity;
+    }
+}
