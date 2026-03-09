7b8d4b021b2fd13ac856dc2c7299aa10cf4c2719
Tomi Karlstedt <tomi.karlstedt@reaktor.com>
2026-03-09 13:27:05 +0200

    OPHYK-1087 KOSKI KIOS and omadata oauth2 kayttooikeus

---
+++ kayttooikeus-service/src/main/resources/db/migration/V20260309112233000__koski_kios_oauth2.sql
@@ -0,0 +1,2 @@
+SELECT insertkayttooikeus('KOSKI', 'KIOS', 'Koski-tietojen lukuoikeus kielitutkintoon osallistuvan henkilön koulutustietojen tarkastamista varten');
+SELECT insertkayttooikeus('KOSKI', 'OMADATAOAUTH2_OPISKELUOIKEUDET_KAIKKI_TIEDOT_JA_VALINTATIEDOT', 'OmaData OAuth2 scope');
