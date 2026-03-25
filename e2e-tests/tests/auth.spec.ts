1cfd5ebb43725b06f111f0dbb55d54849f73023f
Otto Uusihakala <otto.uusihakala@gmail.com>
2026-03-25 13:41:02 +0200

    OPHYK-1047 removed eslint configuration from e2e tests since typescript-eslint doesn't support typescript 6 yet

--- e2e-tests/tests/auth.spec.ts
+++ e2e-tests/tests/auth.spec.ts
@@ -1,35 +1,35 @@
 import { test, expect, Page } from '@playwright/test';
 
 test.describe('Authentication', () => {
-  const username = process.env.TEST_USER || 'testuser';
-  const password = process.env.TEST_PASS || 'password';
+    const username = process.env.TEST_USER || 'testuser';
+    const password = process.env.TEST_PASS || 'password';
 
-  const login = async (page: Page) => {
-    await page.goto('/secure');
+    const login = async (page: Page) => {
+        await page.goto('/secure');
 
-    await page.fill('input[name="username"]', username);
-    await page.fill('input[name="password"]', password);
-    
-    await page.click('button[name="login"]');
-  }
+        await page.fill('input[name="username"]', username);
+        await page.fill('input[name="password"]', password);
 
-  test('should login successfully', async ({ page }) => {
-    await login(page);
+        await page.click('button[name="login"]');
+    };
 
-    // Await for the url to have ticket
-    await page.waitForURL((url) => url.searchParams.has('ticket'));
-    // Check ticket
-    const url = new URL(page.url());
-    expect(url.searchParams.get('ticket')).toMatch(/^ST-/);
-  });
+    test('should login successfully', async ({ page }) => {
+        await login(page);
 
-  test('should logout successfully', async ({ page }) => {
-    await login(page);
-    
-    await page.goto('/logout');
+        // Await for the url to have ticket
+        await page.waitForURL((url) => url.searchParams.has('ticket'));
+        // Check ticket
+        const url = new URL(page.url());
+        expect(url.searchParams.get('ticket')).toMatch(/^ST-/);
+    });
 
-    await page.waitForURL((url) => !url.searchParams.has('ticket'));
-    const url = new URL(page.url());
-    expect(url.searchParams.get('ticket')).toBeNull();
-  });
+    test('should logout successfully', async ({ page }) => {
+        await login(page);
+
+        await page.goto('/logout');
+
+        await page.waitForURL((url) => !url.searchParams.has('ticket'));
+        const url = new URL(page.url());
+        expect(url.searchParams.get('ticket')).toBeNull();
+    });
 });
