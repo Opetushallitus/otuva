666fee420667d970c176e741487644cc6f0e8221
Henry Heikkinen <henry.heikkinen@reaktor.com>
2026-03-30 12:39:10 +0300

    OPHYK-1047 Retry navigation to secured url in e2e tests to deal with CAS lazy loading delegated idp clients

--- e2e-tests/tests/auth.spec.ts
+++ e2e-tests/tests/auth.spec.ts
@@ -1,11 +1,11 @@
-import { test, expect, Page } from '@playwright/test';
+import { expect, Page, test } from '@playwright/test';
 
 test.describe('Authentication', () => {
     const username = process.env.TEST_USER || 'testuser';
     const password = process.env.TEST_PASS || 'password';
 
     const login = async (page: Page) => {
-        await page.goto('/secure');
+        await navigateAndRetryUntilRedirectedToDelegatedIdp(page, '/secure');
 
         await page.fill('input[name="username"]', username);
         await page.fill('input[name="password"]', password);
@@ -33,3 +33,22 @@ test.describe('Authentication', () => {
         expect(url.searchParams.get('ticket')).toBeNull();
     });
 });
+
+async function navigateAndRetryUntilRedirectedToDelegatedIdp(page: Page, url: string) {
+    const MAX_ATTEMPTS = 5;
+    for (let attempt = 1; attempt <= MAX_ATTEMPTS; attempt++) {
+        await page.goto(url);
+        try {
+            await page.waitForURL((url) => isAtDelegatedIdp(url), { timeout: 5000 });
+            return;
+        } catch {
+            if (attempt === MAX_ATTEMPTS) {
+                throw new Error(`Failed to reach Keycloak IdP after ${MAX_ATTEMPTS} attempts. Last URL: ${page.url()}`);
+            }
+        }
+    }
+}
+
+const isAtDelegatedIdp = (url: URL) => {
+    return url.toString().startsWith('http://localhost:8083/realms/test/protocol/saml');
+};
