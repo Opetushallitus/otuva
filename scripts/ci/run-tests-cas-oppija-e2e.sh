5686b9b21a057262af1ba318cebe4099e90661a2
Timo Suomela <timo.suomela@reaktor.com>
2026-04-10 18:45:38 +0300

    OPHYK-1124 start and wait for containers to be healthy at the beginning of e2e tests

--- scripts/ci/run-tests-cas-oppija-e2e.sh
+++ scripts/ci/run-tests-cas-oppija-e2e.sh
@@ -8,7 +8,7 @@ function main {
   local params_for_playwright="${1:-}"
   require_docker
 
-  start_keycloak
+  start_support_containers
   start_mock_substance_service
   start_kayttooikeus
   start_cas_oppija
@@ -16,20 +16,13 @@ function main {
   run_playwright_tests "$params_for_playwright"
 }
 
-function start_keycloak {
-  start_container keycloak
-}
-
 kayttooikeus_backend_pid=""
 
 function start_kayttooikeus {
-  start_container kayttooikeus-db
-
   select_java_version "21"
   cd "$repo"/kayttooikeus-service
 
   ./mvnw clean install -Dmaven.test.skip=true
-  wait_for_container_to_be_healthy kayttooikeus-db
 
   local -r jvm_args=(
     "--add-opens=java.base/java.util=ALL-UNNAMED"
@@ -46,13 +39,10 @@ function start_kayttooikeus {
 mock_substance_service_backend_pid=""
 
 function start_mock_substance_service {
-  start_container mock-substance-service-db
-
   select_java_version "21"
   cd "$repo/mock-substance-service"
 
   ./mvnw clean install -Dmaven.test.skip=true
-  wait_for_container_to_be_healthy otuva-mock-substance-service-db
 
   local -r jvm_args=(
     "--add-opens=java.base/java.util=ALL-UNNAMED"
@@ -68,7 +58,6 @@ function start_mock_substance_service {
 
 function stop_mock_substance_service {
   stop_process mock_substance_service $mock_substance_service_backend_pid
-  stop_container otuva-mock-substance-service-db
 }
 
 function wait_for_backend_to_be_healthy {
@@ -89,18 +78,13 @@ info "Waiting for backend ${name} to start on port ${port}"
 
 function stop_kayttooikeus {
   stop_process kaytoikeus_backend $kayttooikeus_backend_pid
-  stop_container kayttooikeus-db
 }
 
 cas_oppija_backend_pid=""
 
 function start_cas_oppija {
-  start_container cas-oppija-db
-
   select_java_version "21"
   cd "$repo"/cas-oppija
-
-  wait_for_container_to_be_healthy cas-oppija-db
 
   nohup ./gradlew --no-daemon clean run -Dcas.standalone.configurationFile=config/local.yml &
   cas_oppija_backend_pid=$!
@@ -110,7 +94,6 @@ function start_cas_oppija {
 
 function stop_cas_oppija {
   stop_process cas_oppija_backend $cas_oppija_backend_pid
-  stop_container cas-oppija-db
 }
 
 function select_java_version {
@@ -141,14 +124,12 @@ function is_running_on_github_actions {
   [ -n "${GITHUB_ACTIONS:-}" ]
 }
 
-function start_container {
-  local service_name="$1"
-  docker compose up --force-recreate --renew-anon-volumes --detach "$service_name" --wait
+function start_support_containers {
+  docker compose up --force-recreate --renew-anon-volumes --detach --wait
 }
 
-function stop_container {
-  local service_name="$1"
-  docker compose rm --stop --force --volumes "$service_name" >/dev/null 2>&1 || true
+function stop_support_containers {
+  docker compose down --stop --force --volumes >/dev/null 2>&1 || true
 }
 
 function stop_process {
@@ -170,27 +151,7 @@ function cleanup {
   stop_cas_oppija
   stop_mock_substance_service
   stop_kayttooikeus
-  stop_container keycloak
-}
-
-function wait_for_container_to_be_healthy {
-  local -r container_name="$1"
-  local -r timeout_seconds="${2:-60}"
-  local -r start_time=$(date +%s)
-
-  info "Waiting for docker container $container_name to be healthy (timeout: ${timeout_seconds}s)"
-  until [ "$(docker inspect -f {{.State.Health.Status}} "$container_name" 2>/dev/null || echo "not-running")" == "healthy" ]; do
-    local current_time=$(date +%s)
-    local elapsed_time=$((current_time - start_time))
-
-    if [ "$elapsed_time" -ge "$timeout_seconds" ]; then
-      local status=$(docker inspect -f {{.State.Health.Status}} "$container_name" 2>/dev/null || echo "not-running")
-      local logs=$(docker logs "$container_name" --tail 20 2>&1)
-      fatal "Timed out waiting for container $container_name to be healthy. Current status: $status. Last logs:\n$logs"
-    fi
-
-    sleep 2
-  done
+  stop_support_containers
 }
 
 function run_playwright_tests {
