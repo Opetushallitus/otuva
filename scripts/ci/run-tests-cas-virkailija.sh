0d93683603d6f77c0c3d911ea985c8e4d2efd344
Timo Suomela <timo.suomela@reaktor.com>
2026-05-11 12:36:38 +0000

    OPHYK-1268 move to jdk version 25

--- scripts/ci/run-tests-cas-virkailija.sh
+++ scripts/ci/run-tests-cas-virkailija.sh
@@ -4,7 +4,7 @@ source "$( dirname "${BASH_SOURCE[0]}" )/../lib/common-functions.sh"
 
 function main {
   cd "$repo/cas-virkailija"
-  select_java_version "21"
+  select_java_version "25"
   ./gradlew clean build test
 }
 
