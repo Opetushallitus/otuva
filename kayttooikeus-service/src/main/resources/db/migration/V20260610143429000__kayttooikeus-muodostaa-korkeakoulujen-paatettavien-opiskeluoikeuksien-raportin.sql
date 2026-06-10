85bb22c088fa2f744c5263dd4b995574e0006bc2
Timo Suomela <timo.suomela@reaktor.com>
2026-06-10 15:19:11 +0300

    OPHYK-1351 Fix spelling error

--- kayttooikeus-service/src/main/resources/db/migration/V20260610143429000__kayttooikeus-muodostaa-korkeakoulujen-paatettavien-opiskeluoikeuksien-raportin.sql
+++ kayttooikeus-service/src/main/resources/db/migration/V20260610143429000__kayttooikeus-muodostaa-korkeakoulujen-paatettavien-opiskeluoikeuksien-raportin.sql
@@ -1,1 +1,1 @@
-SELECT insertkayttooikeus('OVARA_VIRKAILIJA', 'KK_YOS', 'Saa muodostaa korkeakoulujen päätettävien opiskeluoikeuksien raportin');
+SELECT insertkayttooikeus('OVARA-VIRKAILIJA', 'KK_YOS', 'Saa muodostaa korkeakoulujen päätettävien opiskeluoikeuksien raportin');
