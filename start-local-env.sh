756909c0ab7828d225e49982c147d4f7e7605101
Henry Heikkinen <henry.heikkinen@reaktor.com>
2026-06-23 14:20:49 +0300

    Simplify start-local-env.sh tmux pane layouting

--- start-local-env.sh
+++ start-local-env.sh
@@ -9,42 +9,27 @@ function stop() {
 trap stop EXIT
 
 function main {
+  commands=(
+    "$repo/scripts/run-docker-compose.sh"
+    "$repo/scripts/run-cas-virkailija.sh"
+    "$repo/scripts/run-cas-virkailija.sh"
+    "$repo/scripts/run-cas-oppija.sh"
+    "$repo/scripts/run-kayttooikeus.sh"
+    "$repo/scripts/run-mock-substance-service.sh"
+  )
+
   cd "$repo"
   local -r session="$( basename "$( pwd )" )"
   tmux kill-session -t "$session" || true
   tmux start-server
   tmux new-session -d -s "$session"
-
-  tmux select-pane -t 0
-  tmux splitw -h
-  tmux select-pane -t 0
-  tmux splitw -v
-  tmux select-pane -t 2
-  tmux splitw -v
-  tmux splitw -v
-  # +---+---+
-  # | 0 | 2 |
-  # +---+---+
-  # | 1 | 3 |
-  # +---+---+
-  # |   | 4 |
-  # +---+---+
-
-  tmux select-pane -t 0
-  tmux send-keys "$repo/scripts/run-docker-compose.sh" C-m
-
-  tmux select-pane -t 1
-  tmux send-keys "$repo/scripts/run-cas-virkailija.sh" C-m
-
-  tmux select-pane -t 2
-  tmux send-keys "$repo/scripts/run-cas-oppija.sh" C-m
-
-  tmux select-pane -t 3
-  tmux send-keys "$repo/scripts/run-kayttooikeus.sh" C-m
-
-  tmux select-pane -t 4
-  tmux send-keys "$repo/scripts/run-mock-substance-service.sh" C-m
-
+  for c in "${commands[@]}"; do
+    tmux splitw -v
+    tmux send-keys "$c" C-m
+    tmux select-layout tiled
+  done
+  tmux kill-pane -t 0
+  tmux select-layout tiled
   tmux attach-session -t "$session"
 }
 
