0879d787bb9e40dce6102866b485d861f8c9a4b6
Tomi Karlstedt <tomi.karlstedt@reaktor.com>
2026-07-03 16:26:29 +0300

    OPHYK-1384 add OVARA-VIRKAILIJA kayttooikeus

---
+++ kayttooikeus-service/src/main/resources/db/migration/V20260703112233000__kayttooikeus-ovara-virkailija-hakeneet.sql
@@ -0,0 +1,2 @@
+SELECT insertkayttooikeus('OVARA-VIRKAILIJA', 'KK_HAKENEET', 'Saa muodostaa Korkeakoulujen hakeneet ja paikan vastaanottaneet -raportin');
+SELECT insertkayttooikeus('OVARA-VIRKAILIJA', 'HAKENEET', 'Saa muodostaa Hakeneet ja paikan vastaanottaneet -raportin');
