Opetushallitus/koodisto

OPHYK-1441: aws rightsizing for hahtuva, dev, qa

infra/src/cdk-app.ts
@@ -133,8 +133,8 @@ class ApplicationStack extends cdk.Stack {
       this,
       "TaskDefinition",
       {
-        cpu: 1024,
-        memoryLimitMiB: 2048,
+        cpu: config.taskCpu,
+        memoryLimitMiB: config.taskMemoryMiB,
         runtimePlatform: {
           operatingSystemFamily: ecs.OperatingSystemFamily.LINUX,
           cpuArchitecture: ecs.CpuArchitecture.ARM64,

Opetushallitus/otuva

infra/src/cdk-app.ts
@@ -305,8 +305,8 @@ class ApplicationStack extends cdk.Stack {
       this,
       "TaskDefinition",
       {
-        cpu: 2048,
-        memoryLimitMiB: 4096,
+        cpu: config.kayttooikeusTaskCpu,
+        memoryLimitMiB: config.kayttooikeusTaskMemoryMiB,
         runtimePlatform: {
           operatingSystemFamily: ecs.OperatingSystemFamily.LINUX,
           cpuArchitecture: ecs.CpuArchitecture.ARM64,
@@ -1245,8 +1245,8 @@ class ServiceProviderApplicationStack extends cdk.Stack {
       this,
       "TaskDefinition",
       {
-        cpu: 2048,
-        memoryLimitMiB: 5120,
+        cpu: config.serviceProviderTaskCpu,
+        memoryLimitMiB: config.serviceProviderTaskMemoryMiB,
         runtimePlatform: {
           operatingSystemFamily: ecs.OperatingSystemFamily.LINUX,
           cpuArchitecture: ecs.CpuArchitecture.ARM64,